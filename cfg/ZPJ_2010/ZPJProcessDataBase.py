6f352ca0d9f52d430981651c1125970a96a3038b
Thomas Hauth <thomas.hauth@cern.ch>
2010-10-01 14:17:18 +0000

    Configuration redone

--- cfg/ZPJ_2010/ZPJProcessDataBase.py
+++ cfg/ZPJ_2010/ZPJProcessDataBase.py
@@ -1,5 +1,16 @@
 import ZPJConfiguration
 
+def getDefaultCorrection( data_path ):
+  g_l2_correction_data=["ak5PFJets_Zplusjet:" + data_path + "jec_data/Spring10_L2Relative_AK5PF.txt",
+      "ak7PFJets_Zplusjet:" + data_path + "jec_data/Spring10_L2Relative_AK7PF.txt",
+      "kt4PFJets_Zplusjet:" + data_path + "jec_data/Spring10_L2Relative_KT4PF.txt",
+      "kt6PFJets_Zplusjet:" + data_path + "jec_data/Spring10_L2Relative_KT6PF.txt",
+      "ak5CaloJets_Zplusjet:" + data_path + "jec_data/Spring10_L2Relative_AK5Calo.txt",
+      "ak7CaloJets_Zplusjet:" + data_path + "jec_data/Spring10_L2Relative_AK7Calo.txt",
+      "kt4CaloJets_Zplusjet:" + data_path + "jec_data/Spring10_L2Relative_KT4Calo.txt",
+      "kt6CaloJets_Zplusjet:" + data_path + "jec_data/Spring10_L2Relative_KT6Calo.txt"]
+  return g_l2_correction_data
+
 
 def getDataBaseConfig(  ):
   process = ZPJConfiguration.configuration("Zplusjet_process_data")
@@ -9,17 +20,7 @@ def getDataBaseConfig(  ):
   _is_data = 1
   _json_file= _calib_data_path + "json/Cert_132440-144114_7TeV_StreamExpress_Collisions10_JSON_v3.txt"
   _process_algos=["ak5PF", "ak7PF", "kt4PF", "kt6PF", "ak5Calo", "ak7Calo", "kt4Calo", "kt6Calo"]
-
   
-  _l2_correction_data=["ak5PFJets_Zplusjet:" + _calib_data_path + "jec_data/Spring10_L2Relative_AK5PF.txt",
-      "ak7PFJets_Zplusjet:" + _calib_data_path + "jec_data/Spring10_L2Relative_AK7PF.txt",
-      "kt4PFJets_Zplusjet:" + _calib_data_path + "jec_data/Spring10_L2Relative_KT4PF.txt",
-      "kt6PFJets_Zplusjet:" + _calib_data_path + "jec_data/Spring10_L2Relative_KT6PF.txt",
-      "ak5CaloJets_Zplusjet:" + _calib_data_path + "jec_data/Spring10_L2Relative_AK5Calo.txt",
-      "ak7CaloJets_Zplusjet:" + _calib_data_path + "jec_data/Spring10_L2Relative_AK7Calo.txt",
-      "kt4CaloJets_Zplusjet:" + _calib_data_path + "jec_data/Spring10_L2Relative_KT4Calo.txt",
-      "kt6CaloJets_Zplusjet:" + _calib_data_path + "jec_data/Spring10_L2Relative_KT6Calo.txt"]
-
   p.general = ZPJConfiguration.section ("general",
       is_data = _is_data,
       write_events = "incut", # none, incut, all
@@ -27,12 +28,37 @@ def getDataBaseConfig(  ):
       do_l2_correction = 1,
       do_l3_correction = 0,
       tchain_file_path = "not set",
-      l2_correction_data = _l2_correction_data,
+      l2_correction_data = getDefaultCorrection(_calib_data_path),
       output_path = "not set",
       json_file = _json_file,
       process_algos=_process_algos
   )
   
+  return p
+
+
+def getMCBaseConfig(  ):
+  process = ZPJConfiguration.configuration("Zplusjet_process_data")
+  p=process
+
+  _calib_data_path = "data/"
+  _is_data = 0
+  _json_file= _calib_data_path + "json/Cert_132440-144114_7TeV_StreamExpress_Collisions10_JSON_v3.txt"
+  _process_algos=["ak5PF", "ak7PF", "kt4PF", "kt6PF", "ak5Calo", "ak7Calo", "kt4Calo", "kt6Calo"]
+
+  p.general = ZPJConfiguration.section ("general",
+      is_data = _is_data,
+      write_events = "none", # none, incut, all
+      fixed_weighting = 0,
+      do_l2_correction = 1,
+      do_l3_correction = 0,
+      tchain_file_path = "not set",
+      l2_correction_data = getDefaultCorrection(_calib_data_path),
+      output_path = "not set",
+      json_file = _json_file,
+      process_algos=_process_algos
+  )
+
   return p
 
 
