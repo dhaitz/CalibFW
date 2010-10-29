0e04911c6692fa7d4eae44e8bb070dcd6a04b866
Thomas Hauth <thomas.hauth@cern.ch>
2010-10-29 12:47:53 +0000

    SaveHolder moved to lib

--- interface/PlotCommon.h
+++ interface/PlotCommon.h
@@ -18,15 +18,62 @@ public:
     TString secname = "general";
     m_sPlotFileNamePostfix = p.getString(secname+".plot_file_postfix");
     m_iSkipBinsEnd = p.getInt(secname+".skip_bins_end");
+    
+    std::string sFileName = p.getConfigFileName().Data();
+    
+    size_t lastSlash = sFileName.rfind("/");
+    std::cout << "/ pos " << lastSlash;
+    if (lastSlash != std::string::npos )
+    {
+      m_sConfigFileName = sFileName.substr( lastSlash + 1, std::string::npos);
+    }
+
   }
 
     TString m_sPlotFileNamePostfix;
     int m_iSkipBinsEnd;
-  
+    TString m_sConfigFileName;
+    
 //  private:
 //    PlotEnv();
 };
 
+
+void saveHolder(CanvasHolder &h,
+                vString formats,
+                bool make_log,
+                TString sNamePostfix,
+                TString sPrefix,
+		PlotEnv & env
+ 	      )
+{
+    TString mod_name(h.getTitle());
+    h.setCanvasTitle(mod_name + sNamePostfix + env.m_sPlotFileNamePostfix);
+
+    if (make_log) {
+        TString can_name(h.getTitle());
+        std::cout << "DEBUG: log scale for the canvas " << (can_name+"_log_y").Data() << std::endl;
+        h.setCanvasTitle(can_name+"_log_y");
+        h.setLogY();
+        for (int i=0;i<formats.size();++i) 
+	{
+//             h.draw();
+            h.save(formats[i].Data());
+        }
+    }
+    else
+    {
+	// make needed folder      
+//      h.save("png");
+        for (int i=0;i<formats.size();++i)
+        {
+//         h.draw();
+	    system ( "mkdir plot_out");
+	    system ( ("mkdir plot_out/" + env.m_sConfigFileName).Data() );
+            h.save(formats[i].Data(), "" , ("plot_out/" + env.m_sConfigFileName + "/").Data());
+        }
+    }
+}
 
 
 class Intervals : public std::vector<PtBin>
