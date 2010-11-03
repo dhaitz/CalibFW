3b6588944ab2465645c8f0a5263ea50197c22c78
Thomas Hauth <thomas.hauth@cern.ch>
2010-11-03 16:07:08 +0000

    configs fixed

--- cfg/ZPJ_2010/process_naf_mc_fall10.py
+++ cfg/ZPJ_2010/process_naf_mc_fall10.py
@@ -2,11 +2,11 @@ import ZPJProcessDataBase
 
 p = ZPJProcessDataBase.getMCBaseConfig()
 
-_data_path = "/local/scratch/hauth/data/skim_zjet_fall10/"
-_out_path = "/local/scratch/hauth/data/ZPJ2010/" 
+_data_path = "/afs/naf.desy.de/user/h/hauth/lustre/ZPJ2010/skim_zjet_fall10/"
+_out_path = "/afs/naf.desy.de/user/h/hauth/lustre/ZPJ2010/" 
 
 p.general.tchain_file_path = _data_path + "mc_job61*0to15out.root" 
-p.general.output_path = _out_path + "local_zplusjet_mc_fall10"
+p.general.output_path = _out_path + "zplusjet_mc_fall10"
 p.general.process_algos = ["ak5PF"]
 #p.general.custom_binning = [0,30,60,100,140,220]
 
--- cfg/ZPJ_2010/process_naf_mc_fall10weighted.py
+++ cfg/ZPJ_2010/process_naf_mc_fall10weighted.py
@@ -7,8 +7,9 @@ _out_path = "/afs/naf.desy.de/user/h/hauth/lustre/ZPJ2010/"
 
 p.general.tchain_file_path = _data_path + "mc_job*.root" 
 p.general.output_path = _out_path + "zplusjet_mc_fall10_weighted"
-#p.general.process_algos = ["ak5PF"]
-p.general.custom_binning = [0,25,30,36,43,51,61,73,87,104,124,148,177,212,254,304,364]
+p.general.process_algos = ["ak5PF"]
+p.general.plot_nocuts=1
+#p.general.custom_binning = [0,25,30,36,43,51,61,73,87,104,124,148,177,212,254,304,364]
 p.general.use_event_weight = 1
 
 p.update()
