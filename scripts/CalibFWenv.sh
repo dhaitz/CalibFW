b07935e7264542baa828c4bc3c6f805eeb0a87da
Thomas Hauth <thomas.hauth@cern.ch>
2011-03-08 00:16:17 +0000

    added something to pythonpath

--- scripts/CalibFWenv.sh
+++ scripts/CalibFWenv.sh
@@ -5,6 +5,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/lib:/usr/lib/root/
 export PATH=$PATH:`pwd`/bin:`pwd`/scripts
 export CFGPATH=`pwd`/cfg
 export FWPATH=`pwd`
-export PYTHONPATH=$PYTHONPATH:$CFGPATH:$CFGPATH/Fragments:`pwd`
+export PYTHONPATH=$PYTHONPATH:$CFGPATH:$CFGPATH/Fragments:`pwd`:`pwd`/macros
+
 
 
