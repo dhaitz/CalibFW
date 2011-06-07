0e7c0fcdbb1d260ec1c2489708c680047ad8b340
Joram Berger <joram.berger@cern.ch>
2011-06-07 17:56:58 +0200

    CMSSW update to 4.2.3.

--- Zplusjet/ZplusjetTreeMaker/TreeMaker_joram_mc_cfg.py
+++ Zplusjet/ZplusjetTreeMaker/TreeMaker_joram_mc_cfg.py
@@ -4,7 +4,7 @@ import FWCore.ParameterSet.Config as cms
 import Zplusjet.ZplusjetTreeMaker.Z_collections_noprecuts_cfi as Zs
 import Zplusjet.ZplusjetTreeMaker.Zplusjet_Base_cff as TreeBaseCfg
 
-defaultGlobalTag = "GR_R_42_V12::All"
+defaultGlobalTag = "START42_V12::All"
 useDefaultGlobalTag = 0
 
 # Check if run by grid-control, if not, define some varibles
@@ -16,15 +16,15 @@ try:
 
 except NameError:
     print "NOTE: Not run by grid-control, substituting MC files."
-    TreeBaseCfg.GCFILE_NAMES__= ["file:/scratch/hh/lustre/cms/user/berger/temp/edm_April21ReReco.root"]
+    TreeBaseCfg.GCFILE_NAMES__= ["file:/scratch/hh/lustre/cms/user/berger/temp/edm_MC_423.root"]
     TreeBaseCfg.GCSKIP_EVENTS__ = 0
-    TreeBaseCfg.GCMAX_EVENTS__ = 100
+    TreeBaseCfg.GCMAX_EVENTS__ = 50
     TreeBaseCfg.GCMY_JOBID__ = "23" 
     useDefaultGlobalTag = 1
 
 
 # use Path combining RECO and GEN for processing 
-process = TreeBaseCfg.getBaseConfig(Zs.goodZToMuMuRecoPath, True)
+process = TreeBaseCfg.getBaseConfig(Zs.allgoodZToMuMuPath, False)
 
 if ( useDefaultGlobalTag ):
     process.GlobalTag.globaltag = defaultGlobalTag
