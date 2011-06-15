93bccd71a9ab50da319dc84967e61ee959d9b735
Thomas Hauth <Thomas.Hauth@cern.ch>
2011-06-15 16:25:53 +0200

    New class to encapsulate the Pipelines in a class

---
+++ interface/EventPipelineRunner.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <vector>
+
+#include <boost/noncopyable.hpp>
+
+#include "EventPipeline.h"
+
+namespace CalibFW
+{
+
+class EventPipelineRunner : public boost::noncopyable
+{
+public:
+
+	// apply cuts of pipeline
+	// -> more generic pipeline modifiers
+
+private:
+
+};
+
+}
+
+
