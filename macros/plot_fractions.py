df7ae8cf23cd7e16b901311917106307dca7a64a
Joram Berger <joram.berger@cern.ch>
2011-09-27 17:11:16 +0200

    Add fit to component fraction plots.

--- macros/plot_fractions.py
+++ macros/plot_fractions.py
@@ -10,6 +10,8 @@ import matplotlib.font_manager as font_man
 matplotlib.rc('text', usetex=True) 
 matplotlib.rc('font', size=16)
 import plotBase
+import ROOT
+import math
 
 print "%1.2f Start with settings" % time.clock()
 ### SETTINGS
@@ -21,8 +23,8 @@ bins = [30, 45, 60, 80, 105, 140, 1000]
 
 #### INPUTFILES
 print "%1.2f Open files:" % time.clock()
-fdata = getROOT.OpenFile(plotBase.GetPath() + "data_Sept08.root", (settings.verbosity > 1))
-fmc = getROOT.OpenFile(plotBase.GetPath() + "MC_Sept08.root", (settings.verbosity > 1))
+fdata = getROOT.OpenFile(plotBase.GetPath() + "data_Sept22.root", (settings.verbosity > 1))
+fmc = getROOT.OpenFile(plotBase.GetPath() + "MC_Sept22.root", (settings.verbosity > 1))
 
 def DrawComposition(algoname, bins):
 	"""Plot the components of the leading jet"""
@@ -32,7 +34,7 @@ def DrawComposition(algoname, bins):
 	
 	# Name everything you want and take only the first <nbr> entries of them
 	nbr = 5
-	labels =     ["CHF",    "NEF",    "NHF",    "CEF",    "MF", "PF", "EF"][:nbr]
+	labels =     ["CHF",    "NEF",    "NHF",    "CEF",    r"MF $\,$", "PF", "EF"][:nbr]
 	colours =    ['Orange','LightSkyBlue','YellowGreen','MediumBlue','Darkred', 'yellow','Blue'][:nbr] #['#DF5F5F','#7DB1FF','#4DB870','#94E9FF','#AD3333','yellow','orange'][:nbr]
 	markers =    ['o','x','*','^','d','D','>'][:nbr]
 	components = ["chargedhadron", "neutralem", "neutralhadron", "chargedem", "muon", "photon", "electron"][:nbr]
@@ -48,10 +50,22 @@ def DrawComposition(algoname, bins):
 	barWidth = []
 	for i in range(len(bins)-1):
 		barWidth.append(bins[i+1] - bins[i])
-	# drop the first bin
+	# drop the first bin and calculate the average of data-MC via fit
+	fitd = []
+	fitderr = []
+	fitm = []
+	fitmerr = []
 	for i in range(len(mcG)):
 		mcG[i].dropbin(0)
 		dataG[i].dropbin(0)
+		fitf = ROOT.TF1("fit1", "1*[0]", 1.0, 1000.0)
+		getROOT.SafeGet(fdata,graphnames[i]).Fit(fitf,"QN")
+		fitd.append(fitf.GetParameter(0))
+		fitderr.append(fitf.GetParError(0))
+		getROOT.SafeGet(fmc, graphnames[i]).Fit(fitf, "QN")
+		fitm.append(fitf.GetParameter(0))
+		fitmerr.append(fitf.GetParError(0))
+
 	# calculate the difference between data and MC
 	diff = []
 	for i in range(len(mcG)):
@@ -59,15 +73,14 @@ def DrawComposition(algoname, bins):
 
 	#Show a table of values
 	if True:
-		print "\n      Monte Carlo__" + "_"*7*(len(x)-2) + " Data_________" + "_"*7*(len(x)-2)
+		print "\n      Monte Carlo__" + "_"*7*(len(x)-2) + " Data_________" + "_"*7*(len(x)-2) + " Diff__"
 		print "Bins:",
 		for bn in x + x: print "%6.0f" % bn,
 		for i in range(len(mcG)):
 			print "\n%4s:" % (labels[i]),
 			for y in mcG[i].y + dataG[i].y: print "%1.4f" % (y),
+			print "%+1.2f%% +- %1.2f%%" % ((fitd[i]-fitm[i])*100, (fitderr[i]+fitmerr[i])*100),
 
-#	def stack(histo, bottomhisto): # alternative for explicit map
-#		histo.y = map(lambda a,b: a+b, histo.y, bottomhisto.y)
 	# stack the graphs for both MC and data
 	for i in range(len(mcG)-1):
 		mcG[i+1].y = map(lambda a,b: a+b, mcG[i+1].y,mcG[i].y)
@@ -86,9 +99,9 @@ def DrawComposition(algoname, bins):
 			
 	# MC histograms (begin with the last one)
 	for i in range(len(mcG)-1,-1,-1):
-		plt.bar(x, mcG[i].y, width=barWidth, color=colours[i], edgecolor = None, linewidth=0, label=labels[i])
+		plt.bar(x, mcG[i].y, width=barWidth, color=colours[i], edgecolor = None, linewidth=0, label=r"%s: $%1.3f(%d)\, %1.3f(%d)$" % (labels[i],fitd[i],math.ceil(1000*fitderr[i]),fitm[i], math.ceil(1000*fitmerr[i])))
 		plt.plot(bins, mcG[i].y+[mcG[i].y[-1]], drawstyle='steps-post', color='black',linewidth=1)
-
+	ta.text(0.625,0.43, r"Data $\quad\;$ MC", va='top', ha='left', transform=ta.transAxes)
 	#data points
 	for i in range(len(mcG)-1,-1,-1):
 		plt.errorbar(dataG[i].x, dataG[i].y, dataG[i].yerr, elinewidth=1,
@@ -110,6 +123,7 @@ def DrawComposition(algoname, bins):
 
 	for i in range(len(mcG)):
 		plt.errorbar( mcG[i].x, diff[i], dataG[i].yerr, label = labels[i], fmt = "o", capsize = 2, color = colours[i], zorder=15+i)
+		plt.plot([1.0,1000.0], [fitd[i]-fitm[i]]*2, color=colours[i])
 
 	plt.legend(loc='lower right', numpoints=1)
 	plotBase.Save(tf, algoname + "_fractions_diff", settings, False)
