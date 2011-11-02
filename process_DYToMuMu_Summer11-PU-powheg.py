2764319a5e927ccea27ffdc19c598022b8a317ea
Joram Berger <joram.berger@cern.ch>
2011-11-02 17:35:28 +0100

    New configs with npu and eta ranges

--- process_DYToMuMu_Summer11-PU-powheg.py
+++ process_DYToMuMu_Summer11-PU-powheg.py
@@ -1,24 +1,35 @@
 import sys
-import JsonConfigBase
-import LocalConfigBase
-import subprocess
-import copy
 
-conf = JsonConfigBase.GetMcBaseConfig()
+import JsonConfigBase as base
 
+#getdefaultconfig(data)
+conf = base.GetMcBaseConfig()
+
+#change settings
 conf["Algos"] = ["ak5PFJets", "ak5PFJetsL1L2L3", "ak5PFJetsL1L2L3CHS"]
 conf["InputFiles"] = "/data/berger/data/DYToMuMu-Summer11-powheg/*.root"
-conf["OutputPath"] = "/data/berger/data/powheg_Oct19"
+conf["OutputPath"] = "/data/berger/data/powheg_Nov1"
 
-conf = JsonConfigBase.ExpandDefaultMcConfig( [0, 30, 40, 50, 60, 75, 95, 125, 180, 300, 1000], conf, True )
-JsonConfigBase.ApplyReweighting(conf, 'summer11powheg')
+#setbins()
+conf = base.ExpandDefaultMcConfig(
+        [0, 30, 40, 50, 60, 75, 95, 125, 180, 300, 1000], conf)
+base.ApplyReweighting(conf, 'summer11powheg')
 
-back2back_var = JsonConfigBase.ExpandRange( conf["Pipelines"], "CutBack2Back", [0.24, 0.34,0.44], True, True  ) 
-secjet_var = JsonConfigBase.ExpandRange( conf["Pipelines"], "CutSecondLeadingToZPt", [0.1, 0.15, 0.2, 0.3], True, True  )
+# variations
+back2back_var = base.ExpandRange(conf["Pipelines"], "CutBack2Back", [0.24, 0.34,0.44])
+secjet_var = base.ExpandRange(conf["Pipelines"], "CutSecondLeadingToZPt", [0.1, 0.15, 0.2, 0.3])
+npu_var = base.ExpandRange2(conf["Pipelines"], 'RecoVert', [0, 2, 6], [1, 5, 100])
 
+etabins = [0.0, 0.261, 0.522, 0.783, 0.957, 1.131, 1.305, 1.479, 1.93, 2.322, 2.411, 2.5, 2.853, 2.964, 3.139, 3.489, 5.191]
+jeteta_var = base.ExpandRange2(conf["Pipelines"], 'JetEta', etabins[:-1], etabins[1:])
+
+#included
+conf["Pipelines"] = dict( conf["Pipelines"].items() + npu_var.items())
+conf["Pipelines"] = dict( conf["Pipelines"].items() + jeteta_var.items())
 conf["Pipelines"] = dict( conf["Pipelines"].items() + secjet_var.items())
-conf["Pipelines"] = dict( conf["Pipelines"].items() + JsonConfigBase.CreateEndcapPipelines( conf["Pipelines"] ).items())
+conf["Pipelines"] = dict( conf["Pipelines"].items() + base.CreateEndcapPipelines( conf["Pipelines"] ).items())
 
+#add statistics
 conf["Pipelines"]["default"]["AdditionalConsumer"] = ["cut_statistics"]
 
-JsonConfigBase.Run( conf, sys.argv[0] + ".json")
+base.Run(conf, sys.argv[0] + ".json")
---
+++ process_ZJetToMuMu_Summer11_herwig.py
@@ -0,0 +1,49 @@
+import sys
+import JsonConfigBase
+import LocalConfigBase
+import subprocess
+import copy
+
+conf = JsonConfigBase.GetMcBaseConfig()
+
+conf["Algos"] = ["ak5PFJets", "ak5PFJetsL1", "ak5PFJetsL1CHS", "ak5PFJetsL1L2L3", "ak5PFJetsL1L2L3CHS"] #, "ak7PFJetsL1", "ak7PFJetsL1L2L3"  ]
+conf["InputFiles"] = "/data/berger/data/ZJetToMuMu-Summer11-herwig/*.root"
+conf["OutputPath"] = "/data/berger/data/herwig_Oct10"
+
+conf["UseWeighting"] = 0
+conf = JsonConfigBase.ExpandDefaultMcConfig( [0, 30, 45, 60, 80, 105, 140, 1000], conf, True )
+
+JsonConfigBase.ApplyReweightingSummer11May10ReReco( conf )
+conf["UseWeighting"] = 1
+conf["UseEventWeight"] = 0
+conf["UseGlobalWeightBin"] = 0
+
+
+back2back_var = JsonConfigBase.ExpandRange( conf["Pipelines"], "CutBack2Back", [0.24, 0.34,0.44], True, True  ) 
+secjet_var = JsonConfigBase.ExpandRange( conf["Pipelines"], "CutSecondLeadingToZPt", [0.1, 0.15, 0.2, 0.3], True, True  )
+
+conf["Pipelines"] = dict( conf["Pipelines"].items() + secjet_var.items())
+conf["Pipelines"] = dict( conf["Pipelines"].items() + JsonConfigBase.CreateEndcapPipelines( conf["Pipelines"] ).items())
+
+
+
+#conf["Pipelines"]["default"]["CutSecondLeadingToZPtJet2Threshold"] = 0
+
+#SecondJetNoCut = copy.deepcopy( conf["Pipelines"]["default"] )
+#SecondJetNoCut["FilterInCutIgnored"] = 16
+#SecondJetNoCut["RootFileFolder"] = "NoSecondJetCut"
+
+#conf["Pipelines"]["NoSecondJetCut"] = SecondJetNoCut
+
+#muon_var = JsonConfigBase.ExpandRange( conf["Pipelines"], "CutMuonPt", range(10, 20), True, True  ) 
+#zmass_var = JsonConfigBase.ExpandRange( conf["Pipelines"], "CutZMassWindow", [17, 20, 23], True, True  )
+#muon_var = JsonConfigBase.ExpandRange( conf["Pipelines"], "CutBack2Back", [0.24, 0.34,0.44], True, True  ) 
+#zmass_var = JsonConfigBase.ExpandRange( conf["Pipelines"], "CutSecondLeadingToZPt", [0.1, 0.15, 0.2, 0.3], True, True  )
+
+#conf["Pipelines"] = dict( conf["Pipelines"].items() + secjet_var.items() ) # back2back_var.items() + 
+#conf["Pipelines"] = dict( conf["Pipelines"].items() + zmass_var.items() )
+
+# additional consumer
+conf["Pipelines"]["default"]["AdditionalConsumer"] = ["cut_statistics"]
+
+JsonConfigBase.Run( conf, sys.argv[0] + ".json")
