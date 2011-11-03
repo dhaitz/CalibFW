d00e7c27747f2bc4b487f42bcef4cab13c8a585b
Thomas Hauth <Thomas.Hauth@cern.ch>
2011-11-03 11:09:48 +0100

    updated config files

--- cfg/SkimmingCalibFW/skim_Data_42x_ZJet.py
+++ cfg/SkimmingCalibFW/skim_Data_42x_ZJet.py
@@ -1,8 +1,14 @@
 import FWCore.ParameterSet.Config as cms
-from skim_Base_42x_ZJet import ZJetBase
+from Kappa.Producers.skim_Base_42x_ZJet import ZJetBase
+
+gtag = '@GLOBALTAG@'
+#gtag = 'GR_R_42_V19::All'
 
 process = ZJetBase.getBaseConfig( src_file = 'file:///storage/5/hauth/zpj/test_data/data_DoubleMu_Run2011A_ReReco_May10_AOD.root',
-				  global_tag = 'GR_R_42_V19::All')
+				  global_tag = gtag, 
+				  additional_actives = ['DataMetadata'])
 
 
+#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32( 1 ) )
+#process.kappatuple.verbose = cms.int32(30)
  
--- cfg/SkimmingCalibFW/skim_MC_42x_ZJet.py
+++ cfg/SkimmingCalibFW/skim_MC_42x_ZJet.py
@@ -2,8 +2,11 @@ import FWCore.ParameterSet.Config as cms
 import copy
 
 import FWCore.ParameterSet.Config as cms
-from skim_Base_42x_ZJet import ZJetBase
+from Kappa.Producers.skim_Base_42x_ZJet import ZJetBase
 
-process = ZJetBase.getBaseConfig( src_file = 'file:/scratch/hh/lustre/cms/user/hauth/ZPJ2011/examples/mc_DYToMuMu_M-20_TuneZ2_7TeV-pythia6_AOD.root',
-				  global_tag = 'MC42_V13::All',
+gtag = '@GLOBALTAG@'
+#gtag =  'START42_V13::All'
+
+process = ZJetBase.getBaseConfig( src_file = 'file:///storage/5/hauth/zpj/test_data/mc_DYToMuMu_M-20_TuneZ2_7TeV-pythia6_AOD.root',
+				  global_tag = gtag,
 				  additional_actives = ['GenMetadata', 'Partons'] )
