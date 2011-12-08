35b9a1fd1f23486960b28a4a6cd52b502e78ab16
Thomas Hauth <Thomas.Hauth@cern.ch>
2011-12-08 19:23:25 +0100

    added dumper

---
+++ src/ZJet/Consumer/Dumper.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+#include <math.h>
+
+#include "GlobalInclude.h"
+#include "RootTools/RootIncludes.h"
+
+#include "../ZJetPipeline.h"
+#include "ZJetConsumer.h"
+
+#include "../MetaDataProducer/ZJetCuts.h"
+#include "../Filter/InCutFilter.h"
+
+namespace CalibFW
+{
+
+class Dumper: public ZJetConsumerBase
+{
+public:
+	Dumper() :
+		ZJetConsumerBase()
+		{
+
+		}
+
+	virtual void Init(EventPipeline<ZJetEventData, ZJetMetaData,
+			ZJetPipelineSettings> * pset)
+	{
+	      // hm ...
+	}
+
+	static std::string GetName()
+	{
+		return "dumper";
+	}
+
+	virtual void Finish()
+	{
+	  
+	}
+
+	// this method is only called for events which have passed the filter imposed on the
+	// pipeline
+	virtual void ProcessFilteredEvent(ZJetEventData const& event, ZJetMetaData const& metaData)
+	{
+
+	}
+
+	// this method is called for all events
+	virtual void ProcessEvent(ZJetEventData const& event,
+			ZJetMetaData const& metaData,
+			FilterResult & result)
+	{
+	    std::cout << "## Event" << std::endl << event.GetContent();
+
+	  std::cout << "## MetaData" << std::endl << metaData.GetContent();
+	}
+
+
+	std::map<std::string, unsigned long> m_cutRejected;
+	ZJetPipeline::MetaDataProducerVector m_cuts;
+
+	unsigned long m_conditional2ndJetPtCut;
+	unsigned long m_conditional2ndJetPtCutBase;
+
+	unsigned long m_conditionalDeltaPhiCut;
+	unsigned long m_conditionalDeltaPhiCutBase;
+
+	unsigned long m_eventCount;
+};
+
+
+
+}
+
