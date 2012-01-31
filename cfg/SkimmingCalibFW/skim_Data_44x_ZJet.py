162ac78ea6774c899f5eb5a38251ab988b1825c6
Joram Berger <joram.berger@cern.ch>
2012-01-31 17:49:32 +0100

    Bugfix: Add additional actives to data skim
    Now, trigger objects and lumi metadata is skimmed, too.
    This was missing before.

--- cfg/SkimmingCalibFW/skim_Data_44x_ZJet.py
+++ cfg/SkimmingCalibFW/skim_Data_44x_ZJet.py
@@ -3,5 +3,6 @@ import skim_base as base
 process = base.getBaseConfig(
     'GR_R_44_V13',
     'file:/storage/6/berger/testfiles/data_DoubleMu_ReReco_AOD.root',
+    ['DataMetadata', 'TriggerObjects'],
     maxevents=100
 )
