c4a557a61ffe10f4776f59d5df410cb1e21d1619
Joram Berger <joram.berger@cern.ch>
2012-01-31 17:34:36 +0100

    Kappa folder deleted, files moved.

--- cfg/SkimmingCalibFW/skim_Data_44x_ZJet.py
+++ cfg/SkimmingCalibFW/skim_Data_44x_ZJet.py
@@ -3,4 +3,5 @@ import skim_base as base
 process = base.getBaseConfig(
     'GR_R_44_V13',
     'file:/storage/6/berger/testfiles/data_DoubleMu_ReReco_AOD.root',
-    maxevents=100)
+    maxevents=100
+)
