bb8589727f8068d5b3f667dde83c95115557b75c
Joram Berger <joram.berger@cern.ch>
2012-01-31 18:08:16 +0100

    Remove 42x cmssw configs.

--- cfg/SkimmingCalibFW/skim_Data_44x_ZJet.py
+++ cfg/SkimmingCalibFW/skim_Data_44x_ZJet.py
@@ -1,7 +1,10 @@
-import skim_base as base
+import Kappa.Producers.skim_Base_44x_ZJet as base
+
+#globaltag = '@GLOBALTAG@'
+globaltag = 'GR_R_44_V13'
 
 process = base.getBaseConfig(
-    'GR_R_44_V13',
+    globaltag,
     'file:/storage/6/berger/testfiles/data_DoubleMu_ReReco_AOD.root',
     ['DataMetadata', 'TriggerObjects'],
     maxevents=100
