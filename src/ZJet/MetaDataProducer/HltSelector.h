f99652710f78c782741f1d1bed1e125e5ccae070
Joram Berger <joram.berger@cern.ch>
2012-05-21 15:50:43 +0200

    Consistent indentation in HltSelector.

--- src/ZJet/MetaDataProducer/HltSelector.h
+++ src/ZJet/MetaDataProducer/HltSelector.h
@@ -9,8 +9,7 @@ class HltSelector: public ZJetMetaDataProducerBase
 {
 public:
 
-	HltSelector( stringvector paths )
-	 : m_hltpaths ( paths ), m_verbose ( false )
+	HltSelector(stringvector paths) : m_hltpaths(paths), m_verbose(false)
 	{
 	}
 
@@ -21,76 +20,73 @@ public:
 	virtual bool PopulateGlobalMetaData(ZJetEventData const& data,
 			ZJetMetaData & metaData, ZJetPipelineSettings const& globalSettings) const
 			{
-			    if ( m_verbose )
-			    {
-			        for( std::vector< std::string >::const_iterator it = data.GetDataLumiMetadata()->hltNames.begin();
-                            		it !=  data.GetDataLumiMetadata()->hltNames.end(); ++ it)
-                            	{
-                                	std::cout << *it << std::endl;
-                            	}
-			    }
+				if (m_verbose)
+				{
+					for (std::vector< std::string >::const_iterator it = data.GetDataLumiMetadata()->hltNames.begin();
+						it !=  data.GetDataLumiMetadata()->hltNames.end(); ++ it)
+					{
+						std::cout << *it << std::endl;
+					}
+				}
 
-                bool unprescaledPathFound = false;
-                std::string bestHltName, curName;
+				bool unprescaledPathFound = false;
+				std::string bestHltName, curName;
 
-                if ( m_hltpaths.size() == 0)
-                {
-                	CALIB_LOG_FATAL("No Hlt Trigger path list configured")
-                }
+				if ( m_hltpaths.size() == 0)
+				{
+					CALIB_LOG_FATAL("No Hlt Trigger path list configured")
+				}
 
-                for ( stringvector::const_iterator it = m_hltpaths.begin();
-                        it != m_hltpaths.end();
-                        ++ it )
-                {
-                    curName = metaData.m_hltInfo->getHLTName( *it );
+				for (stringvector::const_iterator it = m_hltpaths.begin();
+					it != m_hltpaths.end();
+					++ it )
+				{
+					curName = metaData.m_hltInfo->getHLTName( *it );
 /*
-                    if (curName.empty())
-                    {
-                           CALIB_LOG_FATAL("HLT Info for " << *it << " is not available in this skim. Be sure to include it !")
-                    }*/
+					if (curName.empty())
+					{
+						CALIB_LOG_FATAL("HLT Info for " << *it << " is not available in this skim. Be sure to include it !")
+					}
+*/
 
-                    if ( m_verbose )
-                    	std::cout << *it << " becomes " << curName << std::endl;
+					if ( m_verbose )
+						std::cout << *it << " becomes " << curName << std::endl;
 
-                    if ( ! curName.empty() )
-                    {
-                    	bestHltName = curName;
-                    	if ( !metaData.m_hltInfo->isPrescaled(curName))
-                    	{
-                    		unprescaledPathFound = true;
-                            break;
-                    	}
-                    }
-                }
+					if (!curName.empty())
+					{
+						bestHltName = curName;
+						if (!metaData.m_hltInfo->isPrescaled(curName))
+						{
+							unprescaledPathFound = true;
+							break;
+						}
+					}
+				}
 
-                if ( ! unprescaledPathFound )
-                {
-			CALIB_LOG( "Available Triggers:" )
+				if ( ! unprescaledPathFound )
+				{
+					CALIB_LOG( "Available Triggers:" )
 
-                       for( std::vector< std::string >::const_iterator it = data.GetDataLumiMetadata()->hltNames.begin();
-                            it !=  data.GetDataLumiMetadata()->hltNames.end(); ++ it)
-                       {
-                            	CALIB_LOG( (*it) << " prescale: " <<  metaData.m_hltInfo->getPrescale( *it ) )
-                       }
+					for(std::vector< std::string >::const_iterator it = data.GetDataLumiMetadata()->hltNames.begin();
+						it !=  data.GetDataLumiMetadata()->hltNames.end(); ++ it)
+					{
+						CALIB_LOG( (*it) << " prescale: " <<  metaData.m_hltInfo->getPrescale( *it ) )
+					}
 
+					CALIB_LOG_FATAL("No unprescaled trigger found for " << bestHltName << ", prescale: " << metaData.m_hltInfo->getPrescale( bestHltName ))
+				}
 
-             		CALIB_LOG_FATAL("No unprescaled trigger found for " << bestHltName << " ,prescale: " << metaData.m_hltInfo->getPrescale( bestHltName ))
-                }
+				if ( m_verbose )
+					std::cout << "selected " << bestHltName << " as best HLT, prescale: " << metaData.m_hltInfo->getPrescale( bestHltName ) << std::endl;
 
-                if ( m_verbose )
-                    std::cout << "selected " << bestHltName << " as best HLT" << " ,prescale: " << metaData.m_hltInfo->getPrescale( bestHltName ) << std::endl;
+				if ( bestHltName.empty() )
+				{
+					CALIB_LOG_FATAL("No Hlt Trigger path found at all!")
+				}
 
-                if ( bestHltName.empty() )
-                {
-                    CALIB_LOG_FATAL("No Hlt Trigger path found at all !")
+				metaData.SetSelectedHlt(bestHltName);
 
-                }
-
-                metaData.SetSelectedHlt( bestHltName );
-
-
-
-                return true;
+				return true;
 			}
 
 	static std::string Name()
