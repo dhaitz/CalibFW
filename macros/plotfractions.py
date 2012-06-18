6c52f05f5f37f3b1d3c590c7ccc469e5d06a6fdd
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2012-06-18 15:04:19 +0200

    Plot jet fractions over eta

--- macros/plotfractions.py
+++ macros/plotfractions.py
@@ -11,21 +11,38 @@ from ROOT import TF1, TGraphErrors
 import getroot
 import plotbase
 
-def getvalues(nickname, f, opt):
+def getvalues(nickname, f, opt, over = 'z_pt', ch={}):
     graph = TGraphErrors()
     fitf = TF1("fit1", "1*[0]", 1.0, 1000.0)
-    changes = plotbase.createchanges(opt)
-    bins = getroot.binstrings(opt.bins)
-    for i in range(len(opt.bins)-2):
-        # read the values from pt-bin folders
-        changes['bin'] = bins[i+1]
-        ojx = getroot.getobjectfromnick('z_pt', f, changes)
-        ojy = getroot.getobjectfromnick(nickname, f, changes)
-        # store them in a root graph
-        graph.SetPoint(i, ojx.GetMean(), ojy.GetMean())
-        graph.SetPointError(i, ojx.GetMeanError(), ojy.GetMeanError())
+    ch = plotbase.createchanges(opt, {})
+
+    # Get list of bin names
+    if over == 'z_pt':
+        bins = getroot.binstrings(opt.bins)
+    elif over == 'jet1_eta':
+        bins = getroot.etastrings(opt.eta)
+
+    # read the values from bin folders & store them in a root graph
+    for i in range(len(bins)):
+        if over == 'z_pt':
+            ch['bin'] = bins[i]
+            ojx = getroot.getobjectfromnick(over, f, ch)
+            ojy = getroot.getobjectfromnick(nickname, f, ch)
+            graph.SetPoint(i, ojx.GetMean(), ojy.GetMean())
+            graph.SetPointError(i, ojx.GetMeanError(), ojy.GetMeanError())
+        elif over == 'jet1_eta':
+            ch['var'] = bins[i]
+            ojx = getroot.getobjectfromnick(over, f, ch)
+            ojy = getroot.getobjectfromnick(nickname, f, ch)
+            if ojx.GetRMS() < opt.eta[i] or ojx.GetRMS() > opt.eta[i+1]:
+                graph.SetPoint(i, 0.5*(opt.eta[i+1]+opt.eta[i]), ojy.GetMean())
+            else:
+                graph.SetPoint(i, ojx.GetRMS(), ojy.GetMean())
+            graph.SetPointError(i, ojx.GetRMS(), ojy.GetMeanError())
+
     # fit a horizontal line
     graph.Fit(fitf,"QN")
+
     # return plot (fit values are stored in the last point of result)
     result = getroot.root2histo(graph)
     result.x.append(0)
@@ -35,7 +52,7 @@ def getvalues(nickname, f, opt):
     return result
 
 
-def fractions(files, opt):
+def fractions(files, opt, over='jet1_eta'):
     """Plot the components of the leading jet"""
     # Name everything you want and take only the first <nbr> entries of them
     nbr = 5
@@ -50,25 +67,33 @@ def fractions(files, opt):
 
     algoname = opt.algorithm + opt.correction
 
+
     # Get list of graphs from inputfiles
     if opt.verbose:
         print "Loading MC Plots"
-    mcG = [getvalues(graphname, files[1], opt) for graphname in graphnames]
+    mcG = [getvalues(graphname, files[1], opt, over) for graphname in graphnames]
     if opt.verbose:
         print mcG[0]
         print "Loading Data Plots"
-    dataG = [getvalues(graphname, files[0], opt) for graphname in graphnames]
+    dataG = [getvalues(graphname, files[0], opt, over) for graphname in graphnames]
     if opt.verbose:
         print "Loading done"
 
     # get x values and bar widths
-    bins = copy.deepcopy(opt.bins)
-    x = bins[1:-1]
-    if bins[0] == 0:
-        bins.pop(0)
-    barWidth = []
-    for i in range(len(bins)-1):
-        barWidth.append(bins[i+1] - bins[i])
+    if over == 'z_pt':
+        bins = copy.deepcopy(opt.bins)
+        x = bins[:-1]
+        barWidth = []
+        for i in range(len(bins)-1):
+            barWidth.append(bins[i+1] - bins[i])
+    elif over == 'jet1_eta':
+        bins = copy.deepcopy(opt.eta)
+        x = bins[:-1]
+        barWidth = []
+        for i in range(len(bins)-1):
+            barWidth.append(bins[i+1] - bins[i])
+
+
     # separate fit values, stored in the last entry (see getvalues)
     fitd = []
     fitderr = []
@@ -89,7 +114,7 @@ def fractions(files, opt):
 
     #Show a table of values
     if True:
-        print "\n      Monte Carlo__" + "_"*7*(len(x)-2) + " Data_________" + "_"*7*(len(x)-2) + " Diff__"
+        print "\n      Monte Carlo__" + "_"*7*(len(x)-1) + " Data_________" + "_"*7*(len(x)-1) + " Diff__"
         print "Bins:",
         for bn in x + x: print "%6.0f" % bn,
         for i in range(len(mcG)):
@@ -114,21 +139,37 @@ def fractions(files, opt):
 
     fig, ax = plotbase.newplot()
     # MC histograms (begin with the last one)
+    """print "\nlen(mcG[0]):  ",len(mcG[0])
+    print "len(DataG[0]):  ",len(dataG[0])
+    print "len(x):  ",len(x)"""
+
     assert len(mcG[0]) == len(x)
     assert len(bins) == len(mcG[0]) + 1
+
+    if over == 'jet1_eta':
+        pre = "abs_"
+    else:
+        pre = ""
+
+
     for i in range(len(mcG)-1,-1,-1):
-        ax.bar(x, mcG[i].y, width=barWidth, color=colours[i], edgecolor = None, linewidth=0,
+        if over == 'z_pt':
+            ax.bar(x, mcG[i].y, width=barWidth, color=colours[i], edgecolor = None, linewidth=0,
             label=r"%s: $%1.3f(%d)\, %1.3f(%d)$" % (labels[i],fitd[i],math.ceil(1000*fitderr[i]),fitm[i], math.ceil(1000*fitmerr[i])))
+        else:
+            ax.bar(x, mcG[i].y, width=barWidth, color=colours[i], edgecolor = None, linewidth=0,label=labels[i])
         ax.plot(bins, mcG[i].y+[mcG[i].y[-1]], drawstyle='steps-post', color='black',linewidth=1)
         ax.errorbar(mcG[i].x, mcG[i].y, mcG[i].yerr, elinewidth=1, lw=0, color='grey')
-    ax.text(0.64,0.36, r"Data $\quad$MC", va='top', ha='left', transform=ax.transAxes)
+    #ax.text(0.64,0.36, r"Data $\quad$MC", va='top', ha='left', transform=ax.transAxes)
+   
+    
     #data points
     for i in range(len(mcG)-1,-1,-1):
         ax.errorbar(dataG[i].x, dataG[i].y, dataG[i].yerr, elinewidth=1,
             marker=markers[i], ms=3, color="black", lw=0, ecolor=None)
-    plotbase.labels(ax, opt, legloc='lower right', frame=True)
-    plotbase.axislabels(ax, 'z_pt_log', 'components')
-    plotbase.Save(fig, "fractions_" + algoname, opt, False)
+    plotbase.labels(ax, opt, legloc='lower left', frame=True)
+    plotbase.axislabels(ax, pre+over, 'components')
+    plotbase.Save(fig, "fractions_" + over+ "_" + algoname, opt, False)
 
     #plot the difference (with MC error neglected)
     fig, ax = plotbase.newplot()
@@ -141,13 +182,22 @@ def fractions(files, opt):
         ax.errorbar( mcG[i].x, diff[i], dataG[i].yerr,
             label=r"%s: $%+1.3f(%d)$" % (labels[i],fitd[i]-fitm[i],math.ceil(1000*(fitderr[i]+fitmerr[i]))),
             fmt="o", capsize=2, color=colours[i], zorder=15+i)
-        ax.plot([1.0,1000.0], [fitd[i]-fitm[i]]*2, color=colours[i])
+        if over == 'z_pt':
+            ax.plot([1.0,1000.0], [fitd[i]-fitm[i]]*2, color=colours[i])
     ax = plotbase.labels(ax, opt, legloc='lower left', frame=True)
-    ax = plotbase.axislabels(ax,'z_pt_log', 'components_diff')
-    plotbase.Save(fig, "fractions_diff_" + algoname, opt, False)
+    ax = plotbase.axislabels(ax, pre+over, 'components_diff')
+    plotbase.Save(fig, "fractions_diff_" + over+ "_" + algoname, opt, False)
 
 
-plots = ['fractions']
+def fractions_zpt (files, opt):
+    fractions(files, opt, over='z_pt')
+
+def fractions_jet1eta (files, opt):
+    fractions(files, opt, over='jet1_eta')
+
+
+plots = ['fractions_zpt', 
+    'fractions_jet1eta']
 
 
 if __name__ == "__main__":
@@ -159,4 +209,4 @@ if __name__ == "__main__":
     fdata = getroot.openfile(sys.argv[1])
     fmc = getroot.openfile(sys.argv[2])
     bins = getroot.getbins(fdata, [])
-    fractions(fdata, fmc, opt=plotbase.options(bins=bins))
+    fractions_zpt(fdata, fmc, opt=plotbase.options(bins=bins))
