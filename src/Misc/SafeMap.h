2b0e2be9656a095342191a3ee15426849fd5878f
Thomas Hauth <Thomas.Hauth@cern.ch>
2012-06-26 11:52:11 +0200

    SafeMap also for boost::ptr_map

--- src/Misc/SafeMap.h
+++ src/Misc/SafeMap.h
@@ -4,6 +4,8 @@
 #include <cassert>
 #include <iostream>
 
+#include <boost/ptr_container/ptr_map.hpp>
+
 namespace CalibFW
 {
 
@@ -11,15 +13,29 @@ template<  class TKey, class TMapType >
 class SafeMap{
 public:
 
-	static TMapType const& Get ( TKey k, std::map< TKey, TMapType> const& m )
+	static TMapType const& GetPtrMap ( TKey const& k, boost::ptr_map< TKey, TMapType> const& m )
 	{
-		if ( m.find( k ) == m.end())
+        typename boost::ptr_map< TKey, TMapType> :: const_iterator it = m.find( k );
+
+		if ( it == m.end())
 		{
 			std::cout << "Item " << k << " can not be found in dictionary " << std::endl;
 			assert(false);
 		}
-		return m.at(k);
+		return *it->second ;
 	}
+
+     static TMapType const& Get ( TKey k, std::map< TKey, TMapType> const& m )
+     {
+               if ( m.find( k ) == m.end())
+
+                {
+                        std::cout << "Item " << k << " can not be found in dictionary " << std::endl;
+                        assert(false);
+                }
+               return m.at(k);
+    }
+
 };
 
 }
