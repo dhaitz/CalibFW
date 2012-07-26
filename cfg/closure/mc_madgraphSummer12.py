2fdf4da34784708ce596648d773e44630c9cf30e
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2012-07-26 14:55:12 +0200

    Updated default data & MC config files

--- cfg/closure/mc_madgraphSummer12.py
+++ cfg/closure/mc_madgraphSummer12.py
@@ -2,24 +2,25 @@ import sys
 import ClosureConfigBase as cbase
 
 conf = cbase.GetMcBaseConfig(run='2012')
-conf["InputFiles"] = cbase.CreateFileList("/storage/6/berger/zpj/kappa52_MC12_madgraph-v3/*.root", sys.argv)
+conf["InputFiles"] = cbase.CreateFileList("/storage/6/berger/zpj/kappa52_MC12/*.root", sys.argv)
 conf["OutputPath"] = "closure_mc_madgraphSummer12"
 
-algorithms = ["AK5PFJets", "AK5PFJetsL1L2L3"]
+algorithms = ["AK5PFJets", "AK5PFJetsL1", "AK5PFJetsL1L2L3"]
 cbase.addCHS(algorithms)
 base_algorithms = ["AK5PFJets", "AK5PFJetsCHS"]
+#conf["GlobalAlgorithms"] = ["AK5PF", "AK5PFchs"]
 
 conf = cbase.ExpandDefaultMcConfig(algorithms, conf)
-cbase.ApplyPUReweighting(conf, "kappa52_MC12_madgraph_190456-193336_8TeV_PromptReco")
+cbase.ApplyPUReweighting(conf, "kappa52_MC12_190456-196531_8TeV_PromptReco")
 
 # create various variations ...
 variations = []
-variations += [ cbase.ExpandRange(conf["Pipelines"], "CutSecondLeadingToZPt", [0.1, 0.15, 0.2, 0.3]) ]
-variations += [ cbase.ExpandRange2(variations[0], "Npv", [0, 3, 6, 12], [2, 5, 11, 100]) ]
-variations += [ cbase.ExpandRange2(variations[0], "JetEta", [0, 0.522, 1.305, 1.930, 2.411, 2.853, 3.139], [0.522, 1.305, 1.930, 2.411, 2.853, 3.139, 5.0]) ]
+variations += [ cbase.ExpandRange(conf["Pipelines"], "CutSecondLeadingToZPt", [0.1, 0.15, 0.2, 0.3], onlyBasicQuantities=True) ]
+variations += [ cbase.ExpandRange2(variations[0], "Npv", [0, 5, 9, 16, 22], [4, 8, 15, 21, 100], onlyBasicQuantities=True) ]
+variations += [ cbase.ExpandRange2(variations[0], "JetEta", [0, 0.783, 1.305, 1.93, 2.5, 2.964, 3.139], [0.783, 1.305, 1.93, 2.5, 2.964, 3.139, 5.191], onlyBasicQuantities=True) ]
 
 cbase.AddCorrectionPlots(conf, base_algorithms, l3residual=False)
-cbase.AddCutConsumer(conf, ["AK5PFJetsL1L2L3", "AK5PFJetsCHSL1L2L3"])
+cbase.AddCutConsumer(conf, ["AK5PFJets", "AK5PFJetsL1", "AK5PFJetsL1L2L3", "AK5PFJetsCHSL1L2L3"])
 
 for v in variations:
     conf["Pipelines"].update(v)
