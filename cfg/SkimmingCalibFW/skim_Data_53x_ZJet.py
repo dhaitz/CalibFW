ed7b5056a8df75b8f7fa4dd16924ab337943d52f
Joram Berger <joram.berger@cern.ch>
2012-07-30 18:30:58 +0200

    Changes for CMSSW 5.3.2.

--- cfg/SkimmingCalibFW/skim_Data_53x_ZJet.py
+++ cfg/SkimmingCalibFW/skim_Data_53x_ZJet.py
@@ -1,9 +1,9 @@
 import Kappa.Producers.skim_Base_53x_ZJet as base
 
 process = base.getBaseConfig(
-    'GR_R_52_V9',
-    'file:/storage/6/berger/testfiles/data_2012A_AOD.root',
+    'GR_R_53_V8',
+    'file:/storage/6/berger/testfiles/data_2012C_AOD.root',
     ['DataMetadata', 'TriggerObjects'],
-    maxevents=2000,
+    maxevents=300,
     residual=True
 )
--- cfg/SkimmingCalibFW/skim_MC_53x_ZJet.py
+++ cfg/SkimmingCalibFW/skim_MC_53x_ZJet.py
@@ -1,7 +1,7 @@
 import Kappa.Producers.skim_Base_53x_ZJet as base
 
 process = base.getBaseConfig(
-    'START52_V11',
+    'START53_V6',
     'file:/storage/6/berger/testfiles/mc_madgraphV9_AOD.root',
     ['GenMetadata', 'Partons'],
     maxevents=500
