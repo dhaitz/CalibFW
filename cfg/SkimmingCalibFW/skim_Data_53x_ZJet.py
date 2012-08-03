9e50aef4935f1ba815fb5d86d8e6cd0966137210
Joram Berger <joram.berger@cern.ch>
2012-08-03 12:02:53 +0200

    New type of Global Tags for data analysis.
    New Global Tags have been anounced on hypernews:
    https://hypernews.cern.ch/HyperNews/CMS/get/physics-validation/1818.html
    This tag is taken from:
    https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideFrontierConditions#ANforPrompt

--- cfg/SkimmingCalibFW/skim_Data_53x_ZJet.py
+++ cfg/SkimmingCalibFW/skim_Data_53x_ZJet.py
@@ -1,7 +1,7 @@
 import Kappa.Producers.skim_Base_53x_ZJet as base
 
 process = base.getBaseConfig(
-    'GR_R_53_V8',
+    'GR_P_V40_AN1',
     'file:/storage/6/berger/testfiles/data_2012C_AOD.root',
     ['DataMetadata', 'TriggerObjects'],
     maxevents=300,
