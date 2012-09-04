580e2bf0fa9e2d7fbb436ae991306c9716cf53ad
Joram Berger <joram.berger@cern.ch>
2012-09-04 16:54:53 +0200

    Changes to the VBF plotting config.

--- macros/plot_vbf.py
+++ macros/plot_vbf.py
@@ -1,26 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Main plotting file
-
-   Usage: python macros/plot.py data.root mc.root
-   For further details: python macros/plot.py -h
-   Idea:
-     1. every plot is a function, grouped in files plot*.py
-        datamc: basic quantities (and cuts)
-        extrapolation: (all extrapolation versions)
-        fractions
-        (systematic: Z-scale, npu, zeitabhaengig, flavour, jet algo,
-         jet size, Calo/PF, Pythia/Herwig, 1-2/more pu, eta (endcap, HF))
-    2. plotbase serves to make things easier including formatting
-    3. getroot does the interaction with ROOT files
-    4. good standard settings:
-       settings hierarchy:
-        (0) plotBase.commandlineOptions defaults are overwritten by
-        (1) arguments in main are overwritten by
-        (2) commandline arguments are overwritten by
-        (3) changes afterwards in main
-
-"""
 import plotbase
 import getroot
 
@@ -31,24 +10,20 @@ import plot_resolution
 
 
 def plot():
-    """Run all plots with the given settings"""
-
-    # settings (1):
     op = plotbase.options(
         files=[
-            "/storage/6/berger/zpj/CalibFW/work/data_2012_vbf/out/closure.root",
-            "/storage/6/berger/zpj/CalibFW/work/mc_madgraphSummer12_vbf/out/closure.root",
+            "/home/berger/CalibFW/work/data_2012_vbf/out/closure_data_2012_vbf.root",
+            "/home/berger/CalibFW/work/mc_madgraphSummer12_vbf/out/closure_mc_madgraphSummer12_vbf.root",
         ],
         algorithm="AK5PFJetsCHS",
         correction="L1L2L3Res",
         labels=["2012 data", "Madgraph"],
         colors=['black', '#19D175'],
         style=["o","f"],
-        lumi=5000.0,#4749.9,
+        lumi=9882.0,
         author="Joram Berger",
         date='today',
         out="out_vbf",
-        npv=[(0, 4), (5, 8), (9, 15), (16, 21), (22, 100)],
         plots=plotdatamc.plots +
               plotresponse.plots +
               plotfractions.plots #+
@@ -56,7 +31,6 @@ def plot():
         )
     module_list = [plotdatamc, plotresponse, plot_resolution, plotfractions]
 
-    # override commandline (3):
     op.normalize = True
 
     print "Using Data file " + op.files[0]
