a570ff8a725690ee8dff9a923d032cbe7b54bf1b
Joram Berger <joram.berger@cern.ch>
2012-09-10 19:58:03 +0200

    Slight modification of the Z calculation.
    This is more consistent with our cuts.

--- src/ZJet/MetaDataProducer/ZJetMetaDataProducer.h
+++ src/ZJet/MetaDataProducer/ZJetMetaDataProducer.h
@@ -2,19 +2,16 @@
 
 // from ROOT
 #include <Math/VectorUtil.h>
-
 #include "../ZJetPipeline.h"
 
 namespace CalibFW
 {
 
-//const double g_kZmass = 91.19;
-
 typedef GlobalMetaDataProducerBase<ZJetEventData, ZJetMetaData, ZJetPipelineSettings>
 		ZJetGlobalMetaDataProducerBase;
 //typedef CutHandler<ZJetEventData , ZJetPipelineSettings > ZJetCutHandler;
-
 //typedef EventConsumerBase<EventResult, ZJetPipelineSettings> ZJetConsumerBase;
+
 
 class ValidMuonProducer: public ZJetGlobalMetaDataProducerBase
 {
@@ -69,6 +66,7 @@ public:
 
 	static std::string Name() { return "valid_muon_producer"; }
 };
+
 
 class ValidJetProducer: public ZJetGlobalMetaDataProducerBase
 {
@@ -156,26 +154,24 @@ public:
 	static std::string Name() { return "valid_jet_producer"; }
 };
 
+
 // need the ValidMuonProducer before
 class ZProducer: public ZJetGlobalMetaDataProducerBase
 {
 public:
-    const double zmassRangeMin;
-    const double zmassRangeMax;
+	const double zmassRangeMin;
+	const double zmassRangeMax;
 
-    ZProducer() : ZJetGlobalMetaDataProducerBase(),
-        zmassRangeMin( 68.0), zmassRangeMax ( 112.0 )
-        {
-
-
-        }
+	ZProducer() : ZJetGlobalMetaDataProducerBase(),
+		zmassRangeMin(71.19), zmassRangeMax(111.19)
+		{}
 
 	virtual bool PopulateGlobalMetaData(ZJetEventData const& data,
 			ZJetMetaData & metaData, ZJetPipelineSettings const& globalSettings) const
 	{
-	    KDataMuons const& valid_muons =  metaData.GetValidMuons();
+		KDataMuons const& valid_muons = metaData.GetValidMuons();
 
-		if  ( valid_muons.size() < 2)
+		if (valid_muons.size() < 2)
 		{
 			// no Z to produce here
 			metaData.SetValidZ(false);
@@ -183,51 +179,46 @@ public:
 		}
 
 		// old code uses	Z_mass_cut = "60.0 < mass < 120.0"
-        std::vector < KDataLV > z_cand;
+		std::vector<KDataLV> z_cand;
 
-        if (valid_muons.size() > 3)
+		if (valid_muons.size() > 3)
 		{
-			CALIB_LOG( "Opps. 4 valid muons ? Skipping Event")
+			CALIB_LOG("Warning: 4 valid muons? Skipping Event.")
 			metaData.SetValidZ(false);
 			return false;
 		}
 
 
-        // create all possible Z combinations
-        // note: if we have more than 3 muons in an event, this may produce double counting
-        for ( unsigned int i = 0; i < valid_muons.size() ; ++ i )
-        {
-            for ( unsigned int j = i + 1; j < valid_muons.size(); ++ j)
-            {
-                //std::cout << std::endl << "Combining muon " << i << " with " << j;
+		// Create all possible Z combinations
+		// Note: If we have more than 3 muons in an event, this may produce double counting
+		for (unsigned int i = 0; i < valid_muons.size() ; ++i)
+		{
+			for (unsigned int j = i + 1; j < valid_muons.size(); ++j)
+			{
+				KDataMuon const& m1 = valid_muons.at(i);
+				KDataMuon const& m2 = valid_muons.at(j);
 
-                KDataMuon const& m1 = valid_muons.at(i);
-                KDataMuon const& m2 = valid_muons.at(j);
+				if (m1.charge + m2.charge == 0)
+				{
+					KDataLV z;
+					z.p4 = m1.p4 + m2.p4;
 
-           		if ((m1.charge + m2.charge) == 0)
-                {
-                    KDataLV z;
-                    //z.p4.Se
-                    z.p4 = (m1.p4 + m2.p4);
-
-                    if (  ( z.p4.mass() > zmassRangeMin ) &&
-                          ( z.p4.mass() < zmassRangeMax ) )
-                    {
-                        z_cand.push_back( z );
-                        // std::cout << std::endl << "Found possible Z with mass " << z.p4.mass();
-                    }
-                    else
-                    {
-                        // std::cout << std::endl << "Dropping Z because of wrong mass " << z.p4.mass() ;
-                    }
-                }
-            }
-        }
+					if (z.p4.mass() > zmassRangeMin && z.p4.mass() < zmassRangeMax)
+					{
+						z_cand.push_back(z);
+						// std::cout << std::endl << "Found possible Z with mass " << z.p4.mass();
+					}
+					else
+					{
+						// std::cout << std::endl << "Dropping Z because of wrong mass " << z.p4.mass();
+					}
+				}
+			}
+		}
 
 		if (z_cand.size() > 1)
 		{
-			CALIB_LOG( " -- more than 1 ZBoson candidate found " << z_cand.size() << " in an event. Is there a Higgs ? how to combine this -- dropping event for now")
-			// drop this event for now
+			CALIB_LOG(" -- more than 1 Z boson candidate found." << z_cand.size() << " in an event. Not sure how to combine this. Dropping event for now.")
 			metaData.SetValidZ(false);
 			return false;
 		}
@@ -238,11 +229,10 @@ public:
 			return false;
 		}
 
-
 		metaData.SetZ(z_cand[0]);
 		metaData.SetValidZ(true);
 
-        return true;
+		return true;
 	}
 
 	static std::string Name() { return "z_producer"; }
