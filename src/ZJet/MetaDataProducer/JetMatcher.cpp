2a9e9083253b757d3d81cccd5fbba7b0bd9b0ff8
Joram Berger <joram.berger@cern.ch>
2012-10-30 17:11:56 +0100

    Bugfix: Jet matching was wrong.
    Before, the gen to reco matching was done the wrong way round and the quantity
    RecoToGen was not calculated as expected.

--- src/ZJet/MetaDataProducer/JetMatcher.cpp
+++ src/ZJet/MetaDataProducer/JetMatcher.cpp
@@ -17,8 +17,8 @@ bool JetMatcher::PopulateGlobalMetaData(ZJetEventData const& data,
 			KDataPFJets& pfJets = metaData.GetPFValidJetCollection("AK5PFJetsL1L2L3");
 			KDataLVs* genJets = data.GetGenJetCollection("AK5GenJets");
 
-			std::vector<int> matching_result = matchSort_Matrix<KDataPFJet, KDataLV>
-					(pfJets, pfJets.size(), *genJets, genJets->size(), 0.3);
+			std::vector<int> matching_result = matchSort_Matrix<KDataLV, KDataPFJet>
+					(*genJets, genJets->size(), pfJets, pfJets.size(), 0.3);
 
 			metaData.m_matchingResults["AK5GenJets"] = matching_result;
 		}
@@ -27,8 +27,8 @@ bool JetMatcher::PopulateGlobalMetaData(ZJetEventData const& data,
 			KDataPFJets& pfJets = metaData.GetPFValidJetCollection("AK7PFJetsL1L2L3");
 			KDataLVs* genJets = data.GetGenJetCollection("AK7GenJets");
 
-			std::vector<int> matching_result = matchSort_Matrix<KDataPFJet, KDataLV>
-					(pfJets, pfJets.size(), *genJets, genJets->size(), 0.3);
+			std::vector<int> matching_result = matchSort_Matrix<KDataLV, KDataPFJet>
+					(*genJets, genJets->size(), pfJets, pfJets.size(), 0.3);
 
 			metaData.m_matchingResults["AK7GenJets"] = matching_result;
 		}
--- src/ZJet/MetaDataProducer/JetMatcher.h
+++ src/ZJet/MetaDataProducer/JetMatcher.h
@@ -13,7 +13,7 @@ class JetMatcher: public ZJetGlobalMetaDataProducerBase
 {
 public:
 	JetMatcher(std::string input_jets1, std::string input_jets2, std::string matching_name):
-		m_jets1(input_jets1), m_jets2(input_jets2) , m_matching_name(matching_name)
+		m_jets1(input_jets1), m_jets2(input_jets2), m_matching_name(matching_name)
 	{}
 
 	JetMatcher(stringvector baseAlgos): m_basealgorithms(baseAlgos) {}
