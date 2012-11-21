d2387f1a2eb764fa04ad7d796e40be22879f15f8
Joram Berger <joram.berger@cern.ch>
2012-11-21 17:19:13 +0100

    Further cleanup of skimming files to the necessary ones.

--- cfg/cmssw/skim_Data_53x_ZJet.py
+++
@@ -1,9 +0,0 @@
-import Kappa.Producers.skim_Base_53x_ZJet as base
-
-process = base.getBaseConfig(
-    'GR_P_V40_AN1',
-    'file:/storage/6/berger/testfiles/data_2012C_AOD.root',
-    ['DataMetadata', 'TriggerObjects'],
-    maxevents=300,
-    residual=True
-)
--- cfg/cmssw/skim_MC_53x_ZJet.py
+++
@@ -1,9 +0,0 @@
-import Kappa.Producers.skim_Base_53x_ZJet as base
-
-process = base.getBaseConfig(
-    'START53_V6',
-    'file:/storage/6/berger/testfiles/mc_madgraphV9_AOD.root',
-    ['GenMetadata', 'GenParticles'],
-    maxevents=500
-)
-
