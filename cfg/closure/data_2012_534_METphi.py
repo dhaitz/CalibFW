3a4e02105d058ea17b5086faab4361a95bcd0d14
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2013-02-01 17:28:55 +0100

    New config files with template

--- cfg/closure/data_2012_534_METphi.py
+++ cfg/closure/data_2012_534_METphi.py
@@ -1,34 +1,7 @@
 import sys
 import ClosureConfigBase as cbase
+import data_template
 
-conf = cbase.BaseConfig('data', '2012')
-conf["InputFiles"] = cbase.CreateFileList("/storage/6/berger/zpj/kappa534_Data12/*.root", sys.argv)
-conf["OutputPath"] = "closure_data2012_534"
+conf = data_template.get_template()
 conf["EnableMetPhiCorrection"] = 1
-
-algorithms = ["AK5PFJets", "AK5PFJetsL1", "AK5PFJetsL1L2L3", "AK5PFJetsL1L2L3Res"]
-cbase.addCHS(algorithms)
-base_algorithms = ["AK5PFJets", "AK5PFJetsCHS"]
-
-conf = cbase.ExpandConfig(algorithms, conf, expandptbins=True, alletaFolder=True, zcutsFolder=True)
-
-# create various variations ...
-variations = []
-variations += [ cbase.ExpandRange(conf["Pipelines"], "CutSecondLeadingToZPt", [0.2, 0.3, 0.35, 0.4], onlyBasicQuantities=False) ]
-variations += [ cbase.ExpandRange2(variations[0], "Npv", [0, 5, 9, 16, 22], [4, 8, 15, 21, 100], onlyBasicQuantities=False, alsoForPtBins=False) ]
-variations += [ cbase.ExpandRange2(variations[0], "JetEta", [0, 0.783, 1.305, 1.93, 2.5, 2.964, 3.139], [0.783, 1.305, 1.93, 2.5, 2.964, 3.139, 5.191], onlyBasicQuantities=False, alsoForPtBins=False, onlyOnIncut=False) ]
-
-cbase.AddCorrectionPlots(conf, base_algorithms, l3residual = True)
-cbase.AddCutConsumer(conf, algorithms)
-cbase.AddHltConsumer(conf, base_algorithms, ["HLT_Mu17_Mu8", "HLT_Mu13_Mu8", "HLT_DoubleMu7", "HLT_DoubleMu5"])
-
-for v in variations:
-    conf["Pipelines"].update(v)
-
-cbase.AddLumiConsumer(conf, ["AK5PFJetsL1L2L3Res", "AK5PFJetsCHSL1L2L3Res"], forIncut=True, forAllevents=True, forIncutVariations=True, forAlleventsVariations=True)
-cbase.AddQuantityPlots(conf, ["AK5PFJetsL1L2L3Res", "AK5PFJetsCHSL1L2L3Res"], forIncut=True, forAllevents=True, forIncutVariations=True, forAlleventsVariations=False)
-
-cbase.Add2DHistograms(conf, ["AK5PFJetsL1L2L3Res", "AK5PFJetsCHSL1L2L3Res"], forIncut=True, forAllevents=True, forIncutVariations=True, forAlleventsVariations=True)
-cbase.Add2DProfiles(conf, ["AK5PFJetsL1L2L3Res", "AK5PFJetsCHSL1L2L3Res"], forIncut=True, forAllevents=True, forIncutVariations=True, forAlleventsVariations=True)
-
 cbase.Run(conf, sys.argv)
--- cfg/closure/mc_madgraphSummer12_534_AK7.py
+++ cfg/closure/mc_madgraphSummer12_534_AK7.py
@@ -1,31 +1,6 @@
 import sys
 import ClosureConfigBase as cbase
+import mc_template
 
-conf = cbase.BaseConfig('mc', '2012')
-conf["InputFiles"] = cbase.CreateFileList("/storage/6/berger/zpj/kappa534_MC12_madgraph/*.root", sys.argv)
-conf["OutputPath"] = "closure_madgraphSummer12_534"
-
-algorithms = ["AK7PFJets", "AK7PFJetsL1", "AK7PFJetsL1L2L3"]
-cbase.addCHS(algorithms)
-base_algorithms = ["AK7PFJets", "AK7PFJetsCHS"]
-
-conf = cbase.ExpandConfig(algorithms, conf, expandptbins=True, alletaFolder=True, zcutsFolder=True)
-
-# create various variations ...
-variations = []
-variations += [ cbase.ExpandRange(conf["Pipelines"], "CutSecondLeadingToZPt", [0.2, 0.3, 0.35, 0.4], onlyBasicQuantities=False) ]
-variations += [ cbase.ExpandRange2(variations[0], "Npv", [0, 5, 9, 16, 22], [4, 8, 15, 21, 100], onlyBasicQuantities=False, alsoForPtBins=False) ]
-variations += [ cbase.ExpandRange2(variations[0], "JetEta", [0, 0.783, 1.305, 1.93, 2.5, 2.964, 3.139], [0.783, 1.305, 1.93, 2.5, 2.964, 3.139, 5.191], onlyBasicQuantities=False, alsoForPtBins=False, onlyOnIncut=False) ]
-
-cbase.AddCorrectionPlots(conf, base_algorithms, l3residual=False)
-cbase.AddCutConsumer(conf, algorithms)
-
-for v in variations:
-    conf["Pipelines"].update(v)
-
-cbase.AddQuantityPlots(conf, ["AK7PFJetsL1L2L3", "AK7PFJetsCHSL1L2L3"], forIncut=True, forAllevents=True, forIncutVariations=True, forAlleventsVariations=False)
-
-cbase.Add2DHistograms(conf, ["AK7PFJetsL1L2L3", "AK7PFJetsCHSL1L2L3"], forIncut=True, forAllevents=True, forIncutVariations=True, forAlleventsVariations=False)
-cbase.Add2DProfiles(conf, ["AK7PFJetsL1L2L3", "AK7PFJetsCHSL1L2L3"], forIncut=True, forAllevents=True, forIncutVariations=True, forAlleventsVariations=False)
-
+conf = mc_template.get_template(algo="AK7PFJets")
 cbase.Run(conf, sys.argv)
