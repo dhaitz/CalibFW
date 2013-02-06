f75aef0d3ee99cfe975c7e39fbe870246adb72ea
Joram Berger <joram.berger@cern.ch>
2013-02-06 17:23:00 +0100

    Rewrite of the plot generation for Mikko.

--- macros/plot_mikko.py
+++ macros/plot_mikko.py
@@ -9,148 +9,51 @@ import plotresponse
 
 """This is the macro to produce Mikko's root files"""
 
-plots = ['response_mikko','ratio_mikko']
+plots = ['mikko_all']
 
-def response_mikko(files, opt):
-    al = opt.algorithm
-    co = opt.correction
-    corr = ["L1L2L3","L1L2L3Res"]
-    alg = ["AK5PFJets","AK5PFJetsCHS"]
-
-    for a in alg:
-        opt.algorithm = a
-        for c in corr:
-            opt.correction = c
-            responseplot_mikko(files, opt, ['bal', 'mpf', 'mpf-raw'],
-                drawextrapolation=False, binborders=True)
-    opt.algorithm = al
-    opt.correction = co
-
-def ratio_mikko(files, opt):
+def mikko_all(files, opt):
     al = opt.algorithm
     co = opt.correction
     corr = ["L1L2L3", "L1L2L3Res"]
     alg = ["AK5PFJets", "AK5PFJetsCHS"]
 
+    f = ROOT.TFile(opt.out + "/plots_mikko.root", "RECREATE")
     for a in alg:
         opt.algorithm = a
         for c in corr:
             opt.correction = c
-            ratioplot_mikko(files, opt, ['bal', 'mpf', 'mpf-raw'],
-                drawextrapolation=False, binborders=True)
+            mikko_oneset(files, opt, ['bal', 'mpf', 'mpf-raw'])
+    f.Close()
     opt.algorithm = al
     opt.correction = co
 
 
+def doPlot(resp, dtype, opt, cut, eta, cu_str, et_str, file1, file2=None):
+    d = {
+        'bal': 'PtBal',
+        'mpf': 'MPF',
+        'mpf-raw': 'MPF-notypeI',
+        'RecoGen': 'RecoToGen_bal',
+    }
+    rgraph = plotresponse.getresponse(resp+'resp', 'zpt', opt, file1, file2, {'var': cut+"_"+eta})
+    string = "_".join(filter(None, [dtype, d[resp], opt.algorithm.replace("AK5PFJets", ""), cu_str, et_str, opt.correction]))
+    rgraph.SetTitle(string)
+    rgraph.SetName(string)
+    rgraph.Write()
 
 
-def responseplot_mikko(files, opt, types, labels=None,
-                 colors=["FireBrick", 'blue', 'green', 'red']*7,
-                 markers=['o', '*', 's']*8,
-                 over='zpt',
-                 binborders=False,
-                 drawextrapolation=False):
+def mikko_oneset(files, opt, types=['bal', 'mpf', 'mpf-raw']):
+    et_strings = ["eta00_13", "eta00_08","eta08_13", "eta13_19", "eta19_25", "eta25_30", "eta30_32", "eta32_52"]
+    cu_strings = ["a10", "a15", "a20", "a30"]
 
-    if labels is None:
-        labels = types
 
-    et_strings = ["_eta00_13", "_eta00_08","_eta08_13", "_eta13_19", "_eta19_25", "_eta25_30", "_eta30_32", "_eta32_52"]
-    cu_strings = ["_a10", "_a15", "_a20", "_a30"]
-
-    f = ROOT.TFile(opt.out + "/responseplots_mikko.root", "RECREATE")
-    for et, et_str in zip(([""]+getroot.etastrings(opt.eta)),et_strings):
+    for et, et_str in zip(([""]+getroot.etastrings(opt.eta)), et_strings):
         for cu, cu_str in zip(getroot.cutstrings(opt.cut), cu_strings):
-            for t, l, m, c, in zip(types, labels, markers, colors):
-                extrapolation = False
-                if t == 'RecoGen':
-                    t = 'RecoToGen_bal'
-                #elif len(t) > 3:
-                #    extrapolation = t[3:]
-                #    t = t[:3]
-                if extrapolation in ['ex', 'data', 'mc', 'datamc']:
-                    extrapolation = 'data'
+            for t in types:
                 if 'Gen' not in t:
-                    rgraph = plotresponse.getresponse(t+'resp', over, opt, files[0], None, {'var': cu+"_"+et}, extrapolation)
-                    plot = getroot.root2histo(rgraph)
-
-                    if t == 'bal':
-                        t1 = 'PtBal'
-                    elif t == 'mpf':
-                        t1 = 'MPF'
-                    elif t == 'mpf-raw':
-                        t1 = 'MPF-notypeI'
-                    string = "data_"+t1+"_"+opt.algorithm+"_"+cu_str+et_str+"_"+opt.correction
-                    string = string.replace("AK5PFJetsCHS_", "CHS")
-                    string = string.replace("_AK5PFJets_", "")
-                    rgraph.SetTitle(string)
-                    rgraph.SetName(string)
-  
-                    f = ROOT.TFile(opt.out + "/rootfiles/" + string + ".root", "RECREATE")
-                    rgraph.Write()
-                    f.Close()
-                if extrapolation == 'data':
-                    extrapolation = 'mc'
-                rgraph = plotresponse.getresponse(t+'resp', over, opt, files[1], None, {'var': cu+"_"+et}, extrapolation)
-                plot = getroot.root2histo(rgraph)
-
-                if t == 'bal':
-                    t = 'PtBal'
-                elif t == 'mpf':
-                    t = 'MPF'
-                elif t == 'mpf-raw':
-                    t = 'MPF-notypeI'
-                string = "mc_"+str(t)+"_"+opt.algorithm+"_"+cu_str+et_str+"_"+opt.correction
-                string = string.replace("AK5PFJetsCHS_", "CHS")
-                string = string.replace("_AK5PFJets_", "")
-                rgraph.SetTitle(string)
-                rgraph.SetName(string)
-
-                rgraph.Write()
-    f.Close()
-
-def ratioplot_mikko(files, opt, types, labels=None,
-                 colors=["FireBrick", 'blue', 'green', 'red']*7,
-                 markers=['o', '*', 's']*8,
-                 over='zpt',
-                 binborders=False,
-                 drawextrapolation=False,
-                 fit=True):
-    #type: bal|mpf[ratio|seperate]
-    if binborders:
-        for x in opt.bins:
-            pass #ax.axvline(x, color='gray')
-
-    if labels is None:
-        labels = types
-
-    et_strings = ["_eta00_13", "_eta00_08","_eta08_13", "_eta13_19", "_eta19_25", "_eta25_30", "_eta30_32", "_eta32_52"]
-    cu_strings = ["_a10", "_a15", "_a20", "_a30"]
-    f = ROOT.TFile(opt.out + "/ratioplots_mikko.root", "RECREATE")
-    for et, et_str in zip(([""]+getroot.etastrings(opt.eta)),et_strings):
-        for cu, cu_str in zip(getroot.cutstrings(opt.cut), cu_strings):
-            for t, l, m, c in zip(types, labels, markers, colors):
-                rgraph = plotresponse.getresponse(t[:3]+'resp', over, opt, files[0], files[1], {'var': cu+"_"+et})
-                if fit:
-                    line, err, chi2, ndf = getroot.fitline(rgraph)
-
-                plot = getroot.root2histo(rgraph)
-                if t == 'bal':
-                    t = 'PtBal'
-                elif t == 'mpf':
-                    t = 'MPF'
-                elif t == 'mpf-raw':
-                    t = 'MPF-notypeI'
-                string = "ratio_"+str(t)+"_"+opt.algorithm+"_"+cu_str+et_str+"_"+opt.correction
-                string = string.replace("AK5PFJetsCHS_", "CHS")
-                string = string.replace("_AK5PFJets_", "")
-                rgraph.SetTitle(string)
-                rgraph.SetName(string)
-
-                rgraph.Write()
-    f.Close()
+                    doPlot(t, "data", opt, cu, et, cu_str, et_str, files[0])
+                    doPlot(t, "ratio", opt, cu, et, cu_str, et_str, files[0], files[1])
+                doPlot(t, "mc", opt, cu, et, cu_str, et_str, files[1])
+    print "Output written to:", opt.out + "/plots_mikko.root"
 
 
-            if over == 'jet1eta':
-                pre = "abs_"
-            else:
-                pre = ""
