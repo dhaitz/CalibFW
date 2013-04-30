17ab46552984c4c1b11397c550e188e413aa3ee9
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2013-04-30 10:45:13 +0200

    MC cfg files for separate PU-reweighting for each run period

---
+++ cfg/closure/mc_madgraphSummer12_537_C.py
@@ -0,0 +1,7 @@
+import sys
+import ClosureConfigBase as cbase
+import mc_template
+
+conf = mc_template.get_template()
+cbase.ApplyPUReweighting(conf, 'kappa534_MC12_madgraph_190456-208686_8TeV_Combined22JanReReco_C')
+cbase.Run(conf, sys.argv)
