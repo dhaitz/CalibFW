3ed705675f351762a60f848ec5b7ebc98b0b0fc1
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2013-05-03 15:21:53 +0200

    separate cfg for data 22JanReReco AB,C,D periods

---
+++ cfg/closure/data_2012C_537.py
@@ -0,0 +1,9 @@
+import sys
+import ClosureConfigBase as cbase
+import data_template
+
+conf = data_template.get_template()
+conf["InputFiles"] = cbase.CreateFileList("/storage/8/dhaitz/Winter13ReReco_2/kappa_2012C*.root", sys.argv)
+conf["OutputPath"] = "closure_data2012_537"
+conf["JsonFile"] = cbase.GetBasePath() + "data/json/Cert_190456-208686_8TeV_Combined22JanReReco_Collisions12_JSON.txt"
+cbase.Run(conf, sys.argv)
