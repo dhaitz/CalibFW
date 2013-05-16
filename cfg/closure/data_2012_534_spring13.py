a6f9305a1d94688ad06b76937b58a3baab3f71b7
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2013-05-16 19:49:13 +0200

    Revert "Spring 13 TEST files"
    This reverts commit d6c64952e2756bb3fd061cae2a4c3e8d809839d1.

--- cfg/closure/data_2012_534_spring13.py
+++
@@ -1,7 +0,0 @@
-import sys
-import ClosureConfigBase as cbase
-import data_template
-
-conf = data_template.get_template()
-conf['Jec'] = cbase.GetBasePath() + "data/jec_data/Spring13_V5_DATA"
-cbase.Run(conf, sys.argv)
