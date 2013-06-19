97ac2b538b43f997cb8b91f3b919de22830175f2
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2013-06-19 14:56:58 +0200

    Remove the static phi-corrected MET

--- src/ZJet/ZJetEventProvider.h
+++ src/ZJet/ZJetEventProvider.h
@@ -17,15 +17,9 @@ void KappaEventProvider<ZJetEventData>::WireEvent(bool phicorrection)
 	m_event.m_jetArea = m_fi.Get<KJetArea>("KT6Area");
 	m_event.m_muons = m_fi.Get<KDataMuons>("muons");
 	m_event.m_pfMet = m_fi.Get<KDataPFMET>("PFMET");
-	if (phicorrection)
-		m_event.m_pfMetChs = m_fi.Get<KDataPFMET>("PFMETCHSPhi");
-	else
-		m_event.m_pfMetChs = m_fi.Get<KDataPFMET>("PFMETCHS");
+	m_event.m_pfMetChs = m_fi.Get<KDataPFMET>("PFMETCHS");
 	m_event.m_pfMetL2L3 = m_fi.Get<KDataPFMET>("ak5PFMETL2L3");
-	if (phicorrection)
-		m_event.m_pfMetChsL2L3 = m_fi.Get<KDataPFMET>("ak5PFMETCHSL2L3phi");
-	else
-		m_event.m_pfMetChsL2L3 = m_fi.Get<KDataPFMET>("ak5PFMETCHSL2L3");
+	m_event.m_pfMetChsL2L3 = m_fi.Get<KDataPFMET>("ak5PFMETCHSL2L3");
 	InitPFJets(m_event, "AK5PFJets");
 	InitPFJets(m_event, "AK5PFJetsCHS");
 
