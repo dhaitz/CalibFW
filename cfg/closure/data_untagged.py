78e01bbb3525d0bab701443cdc68893d2a3baa8a
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2013-09-05 11:47:08 +0200

    Cfg files for untagged jets

---
+++ cfg/closure/data_untagged.py
@@ -0,0 +1,21 @@
+import sys
+import ClosureConfigBase as cbase
+
+def get_template(algo="AK5PFJets"):
+    conf = cbase.BaseConfig('data', '2012')
+    conf["InputFiles"] = cbase.CreateFileList("/storage/6/berger/zpj/kappa539_Data12/*.root", sys.argv)
+    conf['OutputPath'] = __file__.split('/')[-1][:-3]
+
+    algorithms = [algo+"CHSL1L2L3", algo+"CHSL1L2L3Res"]
+    base_algorithms = [algo, algo+"CHS"]
+
+    conf = cbase.ExpandConfig(algorithms, conf, expandptbins=False, addResponse=False)
+    conf['Tagged'] = False
+    conf = cbase.treeconfig(conf)
+    return conf
+
+
+if __name__ == "__main__":
+    """Unit test: doing the plots standalone (not as a module)."""
+    conf = get_template()
+    cbase.Run(conf, sys.argv)
---
+++ cfg/closure/mc_untagged.py
@@ -0,0 +1,23 @@
+import sys
+import ClosureConfigBase as cbase
+
+def get_template(algo="AK5PFJets"):
+    conf = cbase.BaseConfig('mc', '2012')
+    conf["InputFiles"] = cbase.CreateFileList("/storage/6/berger/zpj/kappa539_MC12_madgraph/*.root", 
+sys.argv)
+    conf['OutputPath'] = __file__.split('/')[-1][:-3]
+
+
+    algorithms = [algo+"CHSL1L2L3"]
+    base_algorithms = [algo, algo+"CHS"]
+
+    conf = cbase.ExpandConfig(algorithms, conf, expandptbins=False, addResponse=False)
+    conf['Tagged'] = False
+    conf = cbase.treeconfig(conf)
+    return conf
+
+
+if __name__ == "__main__":
+    """Unit test: doing the plots standalone (not as a module)."""
+    conf = get_template()
+    cbase.Run(conf, sys.argv)
