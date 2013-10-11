c9b0cc7127077d018bd70ba7f66115dc7eb38f78
Joram Berger <joram.berger@cern.ch>
2013-10-11 17:05:07 +0200

    Simplify PU reweighting producer

--- src/ZJet/MetaDataProducer/PuReweightingProducer.h
+++ src/ZJet/MetaDataProducer/PuReweightingProducer.h
@@ -2,13 +2,10 @@
 
 // from ROOT
 #include <Math/VectorUtil.h>
-
 #include "ZJetMetaDataProducer.h"
 
 namespace CalibFW
 {
-
-
 /// Event reweighting depending on pile-up and/or 2nd jet \f$pT\f$.
 /** Modify the event weight depending on the number of true interactions per
     event to fit the estimated distribution in data.
@@ -20,8 +17,8 @@ class PuReweightingProducer: public ZJetGlobalMetaDataProducerBase
 public:
 
 	virtual void PopulateMetaData(ZJetEventData const& data,
-			ZJetMetaData & metaData,
-			ZJetPipelineSettings const& m_pipelineSettings) const
+								  ZJetMetaData& metaData,
+								  ZJetPipelineSettings const& m_pipelineSettings) const
 	{
 		// nothing todo here
 	}
@@ -30,33 +27,33 @@ public:
 
 
 	virtual bool PopulateGlobalMetaData(ZJetEventData const& data,
-			ZJetMetaData & metaData,
-			ZJetPipelineSettings const& m_pipelineSettings) const
+										ZJetMetaData& metaData,
+										ZJetPipelineSettings const& m_pipelineSettings) const
 	{
-		if (!m_pipelineSettings.Global()->GetEnablePuReweighting())
-			return true;
+		double fact = 1.0;
 
-		assert(data.m_geneventmetadata != NULL);
-		double fact = m_pipelineSettings.Global()->GetPuReweighting().at(
-			int(data.m_geneventmetadata->numPUInteractionsTruth + 0.5) );
+		// pu reweighting
+		if (m_pipelineSettings.Global()->GetEnablePuReweighting())
+		{
+			assert(data.m_geneventmetadata != NULL);
+			fact *= m_pipelineSettings.Global()->GetPuReweighting().at(
+						int(data.m_geneventmetadata->numPUInteractionsTruth + 0.5));
+		}
+
+		// 2nd jet pt reweighting
+		if (m_pipelineSettings.Global()->GetEnable2ndJetReweighting() &&
+			metaData.GetValidJetCount(m_pipelineSettings, data, "AK5PFJetsCHSL1L2L3") < 2)
+		{
+			KDataLV* jet2 = metaData.GetValidJet(m_pipelineSettings, data, 1, "AK5PFJetsCHSL1L2L3");
+			// apply a new weight for 2 GeV pt bins
+			if (m_pipelineSettings.Global()->Get2ndJetReweighting().size() > int(jet2->p4.Pt() / 2.0))
+				fact *= m_pipelineSettings.Global()->Get2ndJetReweighting().at(
+							int(jet2->p4.Pt() / 2.0));
+			else
+				fact = 0.0;
+		}
+
 		metaData.SetWeight(metaData.GetWeight() * fact);
-
-		// additional 2nd jet pt reweighting
-		if (!m_pipelineSettings.Global()->GetEnable2ndJetReweighting())
-			return true;
-
-		if (metaData.GetValidJetCount(m_pipelineSettings, data, "AK5PFJetsCHSL1L2L3") < 2)
-			return true;
-
-		KDataLV * jet2 = metaData.GetValidJet(m_pipelineSettings, data, 1, "AK5PFJetsCHSL1L2L3");
-		// apply a new weight for 2 GeV pt bins
-		if (m_pipelineSettings.Global()->Get2ndJetReweighting().size() > int(jet2->p4.Pt() / 2.0))
-			fact = m_pipelineSettings.Global()->Get2ndJetReweighting().at(
-				int(jet2->p4.Pt() / 2.0) );
-		else
-			fact = 0.0f;
-		metaData.SetWeight(metaData.GetWeight() * fact);
-
 		return true;
 	}
 };
