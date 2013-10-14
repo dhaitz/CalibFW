258fee7083556ac28a933b4cdaa7659894ae9a64
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2013-10-14 10:49:47 +0200

    Cfg file and PU weights for herwig sample

---
+++ cfg/closure/mc_herwig.py
@@ -0,0 +1,23 @@
+import sys
+import ClosureConfigBase as cbase
+
+
+def get_template(algo="AK5PFJets"):
+    conf = cbase.BaseConfig('mc', '2012')
+    conf["InputFiles"] = cbase.CreateFileList("/storage/6/berger/zpj/kappa539_MC12_herwig/*.root", sys.argv)
+    conf['OutputPath'] = __file__.split('/')[-1][:-3]
+    cbase.ApplyPUReweighting(conf, 'kappa539_MC12_herwig_190456-208686_8TeV_22Jan2013ReReco')
+    cbase.ApplySampleReweighting(conf)
+
+    algorithms = [algo + "CHSL1L2L3"]
+    base_algorithms = [algo, algo + "CHS"]
+
+    conf = cbase.ExpandConfig(algorithms, conf, expandptbins=False, addResponse=False)
+    conf = cbase.treeconfig(conf)
+    return conf
+
+
+if __name__ == "__main__":
+    """Unit test: doing the plots standalone (not as a module)."""
+    conf = get_template()
+    cbase.Run(conf, sys.argv)
