e6b3a3ac09cd3022f2d94697432c4b49cbbd53bb
Joram Berger <joram.berger@cern.ch>
2013-10-18 16:52:00 +0200

    Apply pep8 to plotdatamc

--- macros/plotdatamc.py
+++ macros/plotdatamc.py
@@ -19,13 +19,13 @@ def datamcplot(quantity, files, opt, fig_axes=(), changes=None, settings=None):
     """Template for all data/MC comparison plots for basic quantities."""
 
     # if no settings are given, create:
-    settings = plotbase.getsettings(opt, changes, settings, quantity)   
-    print "A %s plot is created with the following selection: %s" % (quantity, 
+    settings = plotbase.getsettings(opt, changes, settings, quantity)
+    print "A %s plot is created with the following selection: %s" % (quantity,
                                                           settings['selection'])
 
     if 'flavour' in settings['xynames'][0]:
         settings['rebin'] = 4
-    # create list with histograms from a ttree/tntuple 
+    # create list with histograms from a ttree/tntuple
     datamc, rootobjects = [], []
     settings['events'] = []
     for f in files:
@@ -36,8 +36,8 @@ def datamcplot(quantity, files, opt, fig_axes=(), changes=None, settings=None):
         datamc += [getroot.root2histo(rootobjects[-1], f.GetName(), 1)]
         settings['events'] += [datamc[-1].ysum()]
         if 'flavour' in settings['xynames'][0]:
-            datamc[-1].x = [6.5 if x==20.5 else x for x in datamc[-1].x]
-            datamc[-1].xc = [7 if x==21 else x for x in datamc[-1].xc]
+            datamc[-1].x = [6.5 if x == 20.5 else x for x in datamc[-1].x]
+            datamc[-1].xc = [7 if x == 21 else x for x in datamc[-1].xc]
 
     # if true, create a ratio plot:
     if settings['ratio'] and len(datamc) == 2:
@@ -47,9 +47,9 @@ def datamcplot(quantity, files, opt, fig_axes=(), changes=None, settings=None):
         rootobject = None
 
     # use the argument-given fig/axis or create new one:
-    if settings['subplot']==True: 
+    if settings['subplot'] == True:
         fig, ax = fig_axes
-    else: 
+    else:
         fig, ax = plotbase.newplot(run=settings['run'])
 
     settings['filename'] = plotbase.getdefaultfilename(quantity, opt, settings)
@@ -63,49 +63,49 @@ def datamcplot(quantity, files, opt, fig_axes=(), changes=None, settings=None):
     if settings['run'] == 'diff':
         datamc, ax, offset = runplot_diff(files, datamc, ax, settings, quantity)
     else:
-        offset=0
+        offset = 0
 
     # if true, save as root file:
     if settings['root'] is not False:
         getroot.saveasroot(rootobjects, opt, settings)
         return
 
-    #loop over histograms: scale and plot 
-    for f, l, c, s, rootfile, rootobj in reversed(zip(datamc,settings['labels'],
+    #loop over histograms: scale and plot
+    for f, l, c, s, rootfile, rootobj in reversed(zip(datamc, settings['labels'],
                   settings['colors'], settings['markers'], files, rootobjects)):
         scalefactor = 1
         if 'Profile' in f.classname:
-            scalefactor=1
+            scalefactor = 1
             #s = s.replace('f','o')
         elif settings['normalize']:
             if 'scalefactor' in settings:
                 f.scale(settings['scalefactor'])
-            elif (f.ysum()!=0 and datamc[0].ysum()!=0):
+            elif (f.ysum() != 0 and datamc[0].ysum() != 0):
                 scalefactor = datamc[0].ysum() / f.ysum()
                 f.scale(scalefactor)
             elif settings['lumi'] != None:
                 f.scale(opt.lumi)
 
-        if s=='f':
+        if s == 'f':
             if settings['special_binning']:
-                widths = [(a-b) for a,b in zip(f.x[1:], f.x[:-1])]
+                widths = [(a - b) for a, b in zip(f.x[1:], f.x[:-1])]
                 widths += [0]
             else:
                 widths = (f.x[2] - f.x[1])
-            ax.bar(f.x, f.y, widths, bottom=numpy.ones(len(f.x)) 
+            ax.bar(f.x, f.y, widths, bottom=numpy.ones(len(f.x))
               * 1e-6, yerr=f.yerr, ecolor=c, label=l, fill=True, facecolor=c, edgecolor=c)
         else:
-            ax.errorbar(f.xc, f.y, f.yerr, drawstyle='steps-mid', color=c, 
-                                                    fmt=s, capsize=0 ,label=l)
+            ax.errorbar(f.xc, f.y, f.yerr, drawstyle='steps-mid', color=c,
+                                                    fmt=s, capsize=0, label=l)
 
-        if settings['fit'] is not None and ("MC" not in l or settings['run'] is 
+        if settings['fit'] is not None and ("MC" not in l or settings['run'] is
                                                                     not "diff"):
-            plotbase.fit(ax, quantity, rootobj, settings, c, l, 
+            plotbase.fit(ax, quantity, rootobj, settings, c, l,
                                            datamc.index(f), scalefactor, offset)
 
     # set the axis labels and limits
     plotbase.labels(ax, opt, settings, settings['subplot'])
-    plotbase.axislabels(ax, settings['xynames'][0], settings['xynames'][1], 
+    plotbase.axislabels(ax, settings['xynames'][0], settings['xynames'][1],
                                                             settings=settings)
     plotbase.setaxislimits(ax, settings)
     if settings['xynames'][1] == 'events' and 'y' not in opt.user_options:
@@ -122,10 +122,9 @@ def datamcplot(quantity, files, opt, fig_axes=(), changes=None, settings=None):
 
     if 'flavour' in settings['xynames'][0]:
         ax.set_xlim(-0.5, 7.5)
-        ax.set_xticks([0,1,2,3,4,5,7])
+        ax.set_xticks([0, 1, 2, 3, 4, 5, 7])
         ax.set_xticklabels(['undef.', 'd', 'u', 's', 'c', 'b', 'g'])
         ax.axvline(0.5, color='black', linestyle=':')
-
 
     # save it
     if settings['subplot']:
@@ -142,9 +141,9 @@ def datamcplot(quantity, files, opt, fig_axes=(), changes=None, settings=None):
 
 
 try:
-	datamcplot = profile(datamcplot)
+    datamcplot = profile(datamcplot)
 except NameError:
-	pass  # not running with profiler, that's ok.
+    pass  # not running with profiler, that's ok.
 
 
 def getPUindata(version=''):
@@ -163,7 +162,7 @@ def getPUindata(version=''):
     else:
         print "There are no PU distributions for", version, "defined in plotdatamc."
         exit(0)
-    result.yerr = [math.sqrt(i) for i in result.y] 
+    result.yerr = [math.sqrt(i) for i in result.y]
     result.x = range(len(result))
     result.xc = result.x
     result.normalize()
@@ -171,20 +170,20 @@ def getPUindata(version=''):
     assert len(result) > 10
     return result
 
-def runplot_diff(files, datamc, ax, settings, quantity):      
+
+def runplot_diff(files, datamc, ax, settings, quantity):
 
     settings2 = copy.deepcopy(settings)
     if 'components' in settings['xynames'][1]:
         settings2['x'] = [0, 1.5]
     else:
         settings2['x'] = plotbase.getaxislabels_list(settings['xynames'][1])[:2]
-    
 
     mc = getroot.getobjectfromtree(quantity.split("_")[0], files[1], settings2)
     offset = mc.GetMean()
 
     new_y = []
-    for x_elem, y_elem, yerr_elem in zip(datamc[0].xc, datamc[0].y, 
+    for x_elem, y_elem, yerr_elem in zip(datamc[0].xc, datamc[0].y,
                                                                 datamc[0].yerr):
         if y_elem == 0.0:
             datamc[0].xc.remove(x_elem)
@@ -192,21 +191,20 @@ def runplot_diff(files, datamc, ax, settings, quantity):
             datamc[0].yerr.remove(yerr_elem)
         else:
             new_y.append(y_elem - offset)
-    
+
     datamc[0].y = new_y
-    #ax.axhspan(mc.GetMeanError(),-mc.GetMeanError(), 
+    #ax.axhspan(mc.GetMeanError(),-mc.GetMeanError(),
     #                                    color=settings2['colors'][0], alpha=0.4)
     return datamc, ax, offset
 
 
-
 #Some additional submodules ...
-def datamc_all(quantity, files, opt, fig_axes = (), subplot = False,
-                                             changes = None, settings  = None):
+def datamc_all(quantity, files, opt, fig_axes=(), subplot=False,
+                                             changes=None, settings=None):
     """Plot subplots of one quantity in bins of different variation.
        Loop over the different variations and the different alpha cut values.
 
-       plotbase.getvariationlist gets a list of 'changes' dictionaries, one for each variation bin: 
+       plotbase.getvariationlist gets a list of 'changes' dictionaries, one for each variation bin:
          ch_list = [{'var': 'var_JetEta_0to0_783'}, {'var': 'var_JetEta_0_783to1_305'}, ...]
 
        fig_axes = plotbase.newplot(subplots=n) creates a plot figure (fig_axes[0]) with a list fig_axes[1]
@@ -221,11 +219,12 @@ def datamc_all(quantity, files, opt, fig_axes = (), subplot = False,
         variations = ['npv', 'jet1eta', 'zpt', 'alpha']
 
     subtexts = plotbase.getdefaultsubtexts()
-    
-    # change this
-    run=False
 
-    if quantity in variations: variations.remove(quantity)
+    # change this
+    run = False
+
+    if quantity in variations:
+        variations.remove(quantity)
     for variation in variations:
         for cut, cut_string in zip(opt.cut, getroot.cutstrings(opt.cut)):
             ch_list = plotbase.getvariationlist(variation, opt)
@@ -234,49 +233,55 @@ def datamc_all(quantity, files, opt, fig_axes = (), subplot = False,
                 if variation == 'zpt':
                     ch['var'] = cut_string
                 elif variation is not 'alpha':
-                    ch['var'] = cut_string+"_"+ch['var']
+                    ch['var'] = cut_string + "_" + ch['var']
                 if run:
-                    runplot(quantity, files, opt, changes=ch,fig_axes=(fig_axes[0],ax),subplot=True, log=log, subtext=subtext, rebin=rebin, legloc=legloc, fit=fit)
+                    runplot(quantity, files, opt, changes=ch, fig_axes=(fig_axes[0], ax), subplot=True, log=log, subtext=subtext, rebin=rebin, legloc=legloc, fit=fit)
                 else:
                     datamcplot(quantity, files, opt, changes=ch, subplot=True,
-                                   fig_axes=(fig_axes[0],ax), settings=settings)
+                                   fig_axes=(fig_axes[0], ax), settings=settings)
 
-            if variation == 'alpha': text = " for different "+plotbase.nicetext(variation)+" values "
-            else: text = " in "+plotbase.nicetext(variation)+" bins for "+r"$\alpha$ "+str(cut)+"  "
-            title=plotbase.nicetext(quantity)+text+opt.algorithm+" "+opt.correction
+            if variation == 'alpha':
+                text = " for different " + plotbase.nicetext(variation) + " values "
+            else:
+                text = " in " + plotbase.nicetext(variation) + " bins for " + r"$\alpha$ " + str(cut) + "  "
+            title = plotbase.nicetext(quantity) + text + opt.algorithm + " " + opt.correction
             fig_axes[0].suptitle(title, size='x-large')
 
-            if variation == 'alpha': text = "_bins__"
-            else: text = "_bins__alpha_"+str(cut).replace('.','_')+"__"
-            filename = quantity+"/"+quantity+"_in_"+variation+text+opt.algorithm+opt.correction
-            plotbase.EnsurePathExists(opt.out+"/"+quantity)
+            if variation == 'alpha':
+                text = "_bins__"
+            else:
+                text = "_bins__alpha_" + str(cut).replace('.', '_') + "__"
+            filename = quantity + "/" + quantity + "_in_" + variation + text + opt.algorithm + opt.correction
+            plotbase.EnsurePathExists(opt.out + "/" + quantity)
             plotbase.Save(fig_axes[0], filename, opt)
-            if variation == 'alpha': break
+            if variation == 'alpha':
+                break
+
 
 def ratiosubplot(quantity, files, opt, settings):
 
     fig = plotbase.plt.figure(figsize=[7, 10])
-    ax1 = plotbase.plt.subplot2grid((3,1),(0,0), rowspan=2)
-    ax1.number=1
-    ax2 = plotbase.plt.subplot2grid((3,1),(2,0))
-    ax2.number=2
+    ax1 = plotbase.plt.subplot2grid((3, 1), (0, 0), rowspan=2)
+    ax1.number = 1
+    ax2 = plotbase.plt.subplot2grid((3, 1), (2, 0))
+    ax2.number = 2
     fig.add_axes(ax1)
     fig.add_axes(ax2)
 
-    changes = {'subplot':True}
-    
+    changes = {'subplot': True}
+
     datamcplot(quantity, files, opt, fig_axes=(fig, ax1), changes=changes, settings=settings)
 
-    changes['energy']=None
-    changes['lumi']=None
-    changes['ratio']=True
-    changes['legloc']=False
-    changes['y']=[0.5, 1.5]
-    changes['labels']=['Ratio']
-    changes['xynames']=[settings['xynames'][0], 'datamcratio']
-    
+    changes['energy'] = None
+    changes['lumi'] = None
+    changes['ratio'] = True
+    changes['legloc'] = False
+    changes['y'] = [0.5, 1.5]
+    changes['labels'] = ['Ratio']
+    changes['xynames'] = [settings['xynames'][0], 'datamcratio']
+
     datamcplot(quantity, files, opt, fig_axes=(fig, ax2), changes=changes, settings=settings)
-    
+
     fig.subplots_adjust(hspace=0.05)
     ax1.set_xticks([])
     ax1.set_xlabel("")
@@ -285,19 +290,17 @@ def ratiosubplot(quantity, files, opt, settings):
 
 
 def plotfromdict(datamc, opt, name, blacklist=[]):
-    if len(d_plots[name]) == 0: #emptylist
+    if len(d_plots[name]) == 0:  # emptylist
         datamcplot(name, datamc, opt)
 
-    elif len(d_plots[name]) == 1: #list contains only arguments
-        eval("datamcplot('"+name+"', datamc, opt, "+d_plots[name][0]+")")
+    elif len(d_plots[name]) == 1:  # list contains only arguments
+        eval("datamcplot('" + name + "', datamc, opt, " + d_plots[name][0] + ")")
 
-    elif len(d_plots[name]) == 2: #list contains arguments+function
-        eval(d_plots[name][1]+"('"+name+"', datamc, opt, "+d_plots[name][0]+")")
+    elif len(d_plots[name]) == 2:  # list contains arguments+function
+        eval(d_plots[name][1] + "('" + name + "', datamc, opt, " + d_plots[name][0] + ")")
 
-    elif len(d_plots[name]) == 3: #list contains arguments+function+name
-        eval(d_plots[name][1]+"('"+d_plots[name][2]+"', datamc, opt, "+d_plots[name][0]+")")
-
-
+    elif len(d_plots[name]) == 3:  # list contains arguments+function+name
+        eval(d_plots[name][1] + "('" + d_plots[name][2] + "', datamc, opt, " + d_plots[name][0] + ")")
 
 
 plots = []
