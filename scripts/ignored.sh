0559f4780fb61f71c9c31adda61e3c05af4c6cfc
Joram Berger <joram.berger@cern.ch>
2013-10-24 17:20:44 +0200

    Improve ignored script that lists git ignored files

--- scripts/ignored.sh
+++
@@ -1,8 +0,0 @@
-echo -e "Hidden\t" $(git ignored | grep -e \~ -e "\/\." | wc -l)
-echo -e "root\t" $(git ignored | grep \.root | wc -l)
-echo -e "pyc\t" $(git ignored | grep \.pyc | wc -l)
-echo -e "png\t" $(git ignored | grep \.png | wc -l)
-echo -e "pdf\t" $(git ignored | grep \.pdf | wc -l)
-echo -e "object\t" $(git ignored | grep \.o | wc -l)
-echo "Others:"
-git ignored | grep -v \.root | grep -v \.pyc | grep -v \.png | grep -v \~ | grep -v \.o | grep -v \.pdf | grep -v "\/\."
