b30c9c4e10bbd0d5417676188e0df7a2d845ad73
Joram Berger <joram.berger@cern.ch>
2013-10-30 19:21:03 +0100

    Add new config for mc

--- cfg/artus/mc.py
+++ cfg/artus/mc.py
@@ -1,13 +1,12 @@
 import ArtusConfigBase as base
 
 
-def get_template(algo="AK5PFJets"):
+def config():
     conf = base.BaseConfig('mc', '2012')
     conf["InputFiles"] = "/storage/6/berger/zpj/kappa539_MC12_madgraph_tags/*.root"
-
-    algorithms = [algo + "CHSL1L2L3"]
-    base_algorithms = [algo, algo + "CHS"]
-
-    conf = base.ExpandConfig(algorithms, conf, expandptbins=False, addResponse=False)
-    conf = base.treeconfig(conf)
+    conf = base.expand(conf, ['all', 'zcuts'])
+    #base.pipelinediff(conf)
     return conf
+
+def get_template():
+    return config()
