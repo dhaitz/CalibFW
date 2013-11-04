4c61af03818f0117c4755b65c8d95bb19d0c04b7
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2013-11-04 15:28:59 +0100

    Adapted 2011 cfg files

--- cfg/artus/data11_53xjec.py
+++ cfg/artus/data11_53xjec.py
@@ -4,7 +4,6 @@ import ArtusConfigBase as base
 def config():
     conf = base.BaseConfig('data', '2011')
     conf["InputFiles"] = "/storage/6/berger/zpj/kappa539_Data11/*.root"
-    conf['Jec'] = base.GetBasePath() + "data/jec/Summer13_V5_DATA"
+    conf['Jec'] = base.getPath() + "/data/jec/Summer13_V5_DATA"
     conf = base.expand(conf, ['all', 'zcuts'])
-    #base.pipelinediff(conf)
     return conf
