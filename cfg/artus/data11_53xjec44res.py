c9265eacc95aac1f4e1bf86d01f5539ebd8f2f18
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2013-11-05 11:04:11 +0100

    cfg files for the new JEC set (53x jec with 44x residuals)

---
+++ cfg/artus/data11_53xjec44res.py
@@ -0,0 +1,9 @@
+import ArtusConfigBase as base
+
+
+def config():
+    conf = base.BaseConfig('data', '2011')
+    conf["InputFiles"] = "/storage/6/berger/zpj/kappa539_Data11/*.root"
+    conf['Jec'] = base.getPath() + "/data/jec/Summer13_V5_NEW_DATA"
+    conf = base.expand(conf, ['all', 'zcuts'])
+    return conf
