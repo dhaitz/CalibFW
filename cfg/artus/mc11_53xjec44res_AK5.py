da4ae91875a3c27f7b6bc777f6925abc187b744f
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2013-12-02 14:33:07 +0100

    updated cfg files

---
+++ cfg/artus/mc11_53xjec44res_AK5.py
@@ -0,0 +1,11 @@
+import ArtusConfigBase as base
+
+
+def config():
+    conf = base.BaseConfig('mc', '2011', tagged=False)
+    conf['Pipelines']['default']['JetAlgorithm'] = 'AK5PFJetsL1L2L3'
+    conf["InputFiles"] = "/storage/6/berger/zpj/kappa539_MC11/*.root"
+    conf['Jec'] = base.getPath() + "/data/jec/Summer13_V5_NEW_MC"
+    conf = base.expand(conf, ['all', 'zcuts'])
+    conf['EnableLumiReweighting'] = False
+    return conf
