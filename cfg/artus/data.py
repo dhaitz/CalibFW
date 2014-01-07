ee3739b5941c7428ba8f01834761ef97dfba5268
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-01-07 14:43:45 +0100

    New paths for data and MC skims

--- cfg/artus/data.py
+++ cfg/artus/data.py
@@ -3,6 +3,6 @@ import ArtusConfigBase as base
 
 def config():
     conf = base.BaseConfig('data', '2012')
-    conf["InputFiles"] = "/storage/6/berger/zpj/kappa539_Data12_tags/*.root"
+    conf["InputFiles"] = "/storage/8/berger/zpj/kappa539_Data12_tags/*.root"
     conf = base.expand(conf, ['all', 'zcuts'])
     return conf
--- cfg/artus/mc.py
+++ cfg/artus/mc.py
@@ -3,7 +3,7 @@ import ArtusConfigBase as base
 
 def config():
     conf = base.BaseConfig('mc', '2012')
-    conf["InputFiles"] = "/storage/6/berger/zpj/kappa539_MC12_madgraph_tags/*.root"
+    conf["InputFiles"] = "/storage/a/berger/zpj/kappa539_MC12_madgraph_tags/*.root"
     conf = base.expand(conf, ['all', 'zcuts'])
     return conf
 
