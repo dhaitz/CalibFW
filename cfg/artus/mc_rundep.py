be8ab219dafc109911e6d2c6efc51a9d9b9118c7
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-01-23 15:32:57 +0100

    New rundependent MC with the new Kappa tagging

--- cfg/artus/mc_rundep.py
+++ cfg/artus/mc_rundep.py
@@ -3,7 +3,7 @@ import ArtusConfigBase as base
 
 def config():
     conf = base.BaseConfig('mc', '2012', rundepMC=True)
-    conf["InputFiles"] = "/storage/a/berger/zpj/kappa5313_MC12_madgraph_rundep/*.root"
+    conf["InputFiles"] = "/storage/a/berger/zpj/kappa5313_MC12_madgraph_rundep-2/*.root"
     conf = base.expand(conf, ['all', 'zcuts'])
     return conf
 
