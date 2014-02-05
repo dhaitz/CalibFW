510e01ac8cd33e80d94a20356b1b3969926e9179
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-02-05 18:18:47 +0100

    2011: switch to custom JEC

--- cfg/artus/data11.py
+++ cfg/artus/data11.py
@@ -4,7 +4,7 @@ import ArtusConfigBase as base
 def config():
     conf = base.BaseConfig('data', '2011')
     conf["InputFiles"] = "/storage/a/berger/zpj/kappa5313_Data11_Oct12ReReco/*.root"
-    conf['Jec'] = base.getPath() + "/data/jec/2011Legacy/Legacy53_DATA"
+    conf['Jec'] = base.getPath() + "/data/jec/Summer13_V5_NEW_MOD_DATA"
     conf = base.expand(conf, ['all', 'zcuts'])
     conf['checkKappa'] = "newtagging"
     conf['checkArtus'] = "65a2c8c2"
--- cfg/artus/mc11.py
+++ cfg/artus/mc11.py
@@ -6,7 +6,7 @@ def config():
     conf["InputFiles"] = "/storage/6/berger/zpj/kappa539_MC11/*.root"
     conf['Tagged'] = False
     conf = base.expand(conf, ['all', 'zcuts'])
-    conf['Jec'] = base.getPath() + "/data/jec/2011Legacy/Legacy53_MC"
+    conf['Jec'] = base.getPath() + "/data/jec/Summer13_V5_NEW_MOD_MC"
     conf['checkKappa'] = "47c52f"
     conf['checkArtus'] = "abb490"
     return conf
