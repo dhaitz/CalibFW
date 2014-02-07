9533e58fa073d0198a59d65d8cc4e82a8c611835
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-02-07 11:00:49 +0100

    Do the 2011 analysis without CHS by default

--- cfg/artus/data11.py
+++ cfg/artus/data11.py
@@ -5,7 +5,7 @@ def config():
     conf = base.BaseConfig('data', '2011')
     conf["InputFiles"] = "/storage/a/berger/zpj/kappa5313_Data11_Oct12ReReco/*.root"
     conf['Jec'] = base.getPath() + "/data/jec/Summer13_V5_NEW_MOD_DATA"
-    conf = base.expand(conf, ['all', 'zcuts'])
+    conf = base.expand(conf, ['all', 'zcuts'], algorithms=['AK5PFJetsL1L2L3', 'AK5PFJetsL1L2L3Res'])
     conf['checkKappa'] = "newtagging"
     conf['checkArtus'] = "65a2c8c2"
     return conf
