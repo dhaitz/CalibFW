c8cd17bddbc0b9504d89bd8a18ea3e1e048af219
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-02-07 11:00:49 +0100

    Do the 2011 analysis without CHS by default

--- cfg/artus/mc11.py
+++ cfg/artus/mc11.py
@@ -4,9 +4,9 @@ import ArtusConfigBase as base
 def config():
     conf = base.BaseConfig('mc', '2011')
     conf["InputFiles"] = "/storage/6/berger/zpj/kappa539_MC11/*.root"
-    conf['Tagged'] = False
-    conf = base.expand(conf, ['all', 'zcuts'])
     conf['Jec'] = base.getPath() + "/data/jec/Summer13_V5_NEW_MOD_MC"
+    conf['Tagged'] = False
+    conf = base.expand(conf, ['all', 'zcuts'], algorithms=['AK5PFJetsL1L2L3'])
     conf['checkKappa'] = "47c52f"
     conf['checkArtus'] = "abb490"
     return conf
