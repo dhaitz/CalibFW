673e417558e7f5209966bd8a4155a9d9dc13ee19
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-02-14 10:00:03 +0100

    Switch to latest 2011 JEC files from Hartmut

--- cfg/artus/mc11.py
+++ cfg/artus/mc11.py
@@ -4,7 +4,7 @@ import ArtusConfigBase as base
 def config():
     conf = base.BaseConfig('mc', '2011')
     conf["InputFiles"] = "/storage/6/berger/zpj/kappa539_MC11/*.root"
-    conf['Jec'] = base.getPath() + "/data/jec/Summer13_V5_NEW_MOD_MC"
+    conf['Jec'] = base.getPath() + "/data/jec/2011Legacy_NEW/Legacy11_V1_MC"
     conf['Tagged'] = False
     conf = base.expand(conf, ['all', 'zcuts'], algorithms=['AK5PFJetsL1L2L3'])
     conf['checkKappa'] = "47c52f"
