e2b2e7c2f9d05c8a7e3891940db02865caff7d9c
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-02-17 10:15:52 +0100

    Adapt ee data cfg to new skim

--- cfg/artus/data_ee.py
+++ cfg/artus/data_ee.py
@@ -3,10 +3,8 @@ import ArtusConfigBase as base
 
 def config():
     conf = base.BaseConfig('data', '2012', analysis='zee')
-    conf["InputFiles"] = "/storage/a/dhaitz/skims/2014_01_31_zee_data/*.root"
-    conf['GlobalProducer'].remove('hlt_selector')
-    conf['Pipelines']['default']['Filter'].remove('hlt')
-    conf['checkKappa'] = "73f0d43"
-    #conf['checkArtus'] = "e2658c"
+    conf["InputFiles"] = "/storage/a/dhaitz/skims/2014_02_14_zee_data/*.root"
+    #conf['GlobalProducer'].remove('hlt_selector')
+    #conf['Pipelines']['default']['Filter'].remove('hlt')
     conf = base.expand(conf, ['all', 'zcuts'])
     return conf
