9e8a5011fdf96c7d7886949085c8c85ae05e025d
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-03-10 10:29:43 +0100

    New jec files for rundep MC

--- cfg/artus/mc_rundep.py
+++ cfg/artus/mc_rundep.py
@@ -5,6 +5,7 @@ def config():
     conf = base.BaseConfig('mc', '2012', rundepMC=True)
     conf["InputFiles"] = "/storage/a/berger/zpj/kappa5313_MC12_madgraph_rundep-2/*.root"
     conf = base.expand(conf, ['all', 'zcuts'])
+    conf['Jec'] = base.getPath() + "/data/jec/Winter14/Winter14_V1_MC"
     conf['checkKappa'] = "newtagging"
     conf['checkArtus'] = "65a2c8c2"
     return conf
