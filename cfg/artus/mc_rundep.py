ff20ebb5a7202c93830360ab79e8bf23589e4c6a
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-03-14 09:15:24 +0100

    Small changen in rundep MC cfg file

--- cfg/artus/mc_rundep.py
+++ cfg/artus/mc_rundep.py
@@ -8,6 +8,6 @@ def config():
     conf['Jec'] = base.getPath() + "/data/jec/Winter14/Winter14_V1_MC"
     conf['PileupWeights'] = base.getPath() + "/data/pileup/weights_190456-208686_8TeV_22Jan2013ReReco_kappa5313_MC12_madgraph_rundep-2.root"
     conf['checkKappa'] = "newtagging"
-    conf['checkArtus'] = "65a2c8c2"
+    #conf['checkArtus'] = "65a2c8c2"
     return conf
 
