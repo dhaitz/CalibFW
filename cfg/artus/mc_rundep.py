97d34df30d55668197a7e9b35729bd6bca41c4d0
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-03-19 14:21:15 +0100

    New PU weights for RD MC (excluding the runs flagged bad by Mikko)

--- cfg/artus/mc_rundep.py
+++ cfg/artus/mc_rundep.py
@@ -6,6 +6,7 @@ def config():
     conf["InputFiles"] = "/storage/a/berger/zpj/kappa5313_MC12_madgraph_rundep-2/*.root"
     conf = base.expand(conf, ['all', 'zcuts'])
     conf['Jec'] = base.getPath() + "/data/jec/Winter14/Winter14_V1_MC"
+    conf['PileupWeights'] = base.getPath() + "/data/pileup/weights_190456-208686_8TeV_22Jan2013ReReco_kappa5313_MC12_madgraph_rundep-2.root"
     conf['checkKappa'] = "newtagging"
     conf['checkArtus'] = "65a2c8c2"
     return conf
