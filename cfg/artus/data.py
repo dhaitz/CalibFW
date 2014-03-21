c1a0e8cf4c4eadc6e2f602d4e3d7fa5718b8b773
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-03-21 16:41:16 +0100

    Revert "Add L1(only) pipeline"
    This reverts commit 40bf284bed8509466247f06dd5c7bb5eba75b4e6.

--- cfg/artus/data.py
+++ cfg/artus/data.py
@@ -1,18 +1,8 @@
 import ArtusConfigBase as base
-import copy
 
 
 def config():
     conf = base.BaseConfig('data', '2012')
     conf["InputFiles"] = "/storage/8/berger/zpj/kappa539_Data12_tags/*.root"
     conf = base.expand(conf, ['all', 'zcuts'])
-    for pipeline in conf['Pipelines'].keys():
-        if pipeline.startswith('incut_') and pipeline.endswith('L1L2L3'):
-            newpipeline = pipeline.replace('L2L3', '')
-            conf['Pipelines'][newpipeline] = copy.deepcopy(conf['Pipelines'][pipeline])
-            conf['Pipelines'][newpipeline]['JetAlgorithm'] = conf['Pipelines'][pipeline]['JetAlgorithm'].replace('L2L3', '')
-
-            newpipeline = pipeline.replace('L1L2L3', '')
-            conf['Pipelines'][newpipeline] = copy.deepcopy(conf['Pipelines'][pipeline])
-            conf['Pipelines'][newpipeline]['JetAlgorithm'] = conf['Pipelines'][pipeline]['JetAlgorithm'].replace('L1L2L3', '')
     return conf
