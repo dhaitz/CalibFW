2fcd325f38ca93ec4adf13abb665a525309a60a2
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-03-21 16:43:44 +0100

    Changes in data cfg: jec and checkKappa

--- cfg/artus/data.py
+++ cfg/artus/data.py
@@ -5,4 +5,6 @@ def config():
     conf = base.BaseConfig('data', '2012')
     conf["InputFiles"] = "/storage/8/berger/zpj/kappa539_Data12_tags/*.root"
     conf = base.expand(conf, ['all', 'zcuts'])
+    conf['Jec'] = base.getPath() + "/data/jec/Spring14_V1_DATA"
+    conf['checkKappa'] = "master"
     return conf
