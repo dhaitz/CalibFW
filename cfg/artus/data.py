e8b5a36c3a5b9303ce709c8507c3c4cca291fdff
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-03-26 18:34:25 +0100

    New skim for DoubleMu Data

--- cfg/artus/data.py
+++ cfg/artus/data.py
@@ -3,6 +3,7 @@ import ArtusConfigBase as base
 
 def config():
     conf = base.BaseConfig('data', '2012')
-    conf["InputFiles"] = "/storage/8/berger/zpj/kappa539_Data12_tags/*.root"
+    #conf["InputFiles"] = "/storage/8/berger/zpj/kappa539_Data12_tags/*.root"
+    conf["InputFiles"] = "/storage/a/dhaitz/skims/2014_03_25_data12/*.root"
     conf = base.expand(conf, ['all', 'zcuts'])
     return conf
