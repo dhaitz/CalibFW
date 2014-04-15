1dd3c93c3170df8dac21d322c009110e68dde170
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-04-15 15:13:12 +0200

    Latest DiMuon data skim is now on fhgfs

--- cfg/artus/data.py
+++ cfg/artus/data.py
@@ -4,7 +4,7 @@ import ArtusConfigBase as base
 def config():
     conf = base.BaseConfig('data', '2012')
     #conf["InputFiles"] = "/storage/8/berger/zpj/kappa539_Data12_tags/*.root"
-    conf["InputFiles"] = "/storage/a/dhaitz/skims/2014_03_25_data12/*.root"
+    conf["InputFiles"] = "/storage/fhgfs/dhaitz/skims/2014_03_25_data12/*.root"
     conf = base.expand(conf, ['all', 'zcuts'])
     conf['Jec'] = base.getPath() + "/data/jec/Winter14/Winter14_V1_DATA"
     return conf
