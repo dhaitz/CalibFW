dba973e3ca8487d392aef7f0f493ee5d2b484193
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-05-16 13:54:33 +0200

    Herwig branch: adjust cfg

--- cfg/artus/mc_herwig.py
+++ cfg/artus/mc_herwig.py
@@ -4,7 +4,7 @@ import ArtusConfigBase as base
 def config():
     conf = base.BaseConfig('mc', '2012')
     conf["InputFiles"] = "/storage/6/berger/zpj/kappa539_MC12_herwig/*.root"
-    base.ApplyPUReweighting(conf, 'kappa539_MC12_herwig_190456-208686_8TeV_22Jan2013ReReco')
+    #base.ApplyPUReweighting(conf, 'kappa539_MC12_herwig_190456-208686_8TeV_22Jan2013ReReco')
     base.ApplySampleReweighting(conf)
     conf = base.expand(conf, ['all', 'zcuts'])
     return conf
