ec4d7724ba25b4dbda0e4882dc76d025c45f3623
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-06-04 16:07:47 +0200

    Make the plot_mikko function work again

--- plotting/plot_mikko.py
+++ plotting/plot_mikko.py
@@ -11,49 +11,57 @@ import plotresponse
 
 plots = ['mikko_all']
 
-def mikko_all(files, opt):
-    al = opt.algorithm
-    co = opt.correction
-    corr = ["L1L2L3", "L1L2L3Res"]
-    alg = ["AK5PFJets", "AK5PFJetsCHS"]
 
-    f = ROOT.TFile(opt.out + "/plots_mikko.root", "RECREATE")
-    for a in alg:
-        opt.algorithm = a
-        for c in corr:
-            opt.correction = c
-            mikko_oneset(files, opt, ['bal', 'mpf', 'mpf-raw'])
+def mikko_all(files, opt):
+    if opt.filename is None:
+        opt.filename = plots_mikko.root
+    f = ROOT.TFile(opt.out + "/" + opt.filename, "RECREATE")
+    mikko_oneset(files, opt, ['balresp', 'mpfresp', 'mpf-rawresp',
+         'recogen'])
     f.Close()
-    opt.algorithm = al
-    opt.correction = co
 
 
 def doPlot(resp, dtype, opt, cut, eta, cu_str, et_str, file1, file2=None):
     d = {
-        'bal': 'PtBal',
-        'mpf': 'MPF',
-        'mpf-raw': 'MPF-notypeI',
-        'RecoGen': 'RecoToGen_bal',
+        'balresp': 'PtBal',
+        'mpfresp': 'MPF',
+        'mpf-rawresp': 'MPF-notypeI',
+        'recogen': 'RecoToGen',
     }
-    rgraph = plotresponse.getresponse(resp+'resp', 'zpt', opt, file1, file2, {'var': cut+"_"+eta})
-    string = "_".join(filter(None, [dtype, d[resp], opt.algorithm.replace("AK5PFJets", ""), cu_str, et_str, opt.correction]))
-    rgraph.SetTitle(string)
-    rgraph.SetName(string)
-    rgraph.Write()
+
+    changes = {'allalpha': True,
+               'alleta': True,
+               'selection': '(%s) && (%s)' % (cut, eta)
+               }
+    for corr in ["L1L2L3", "L1L2L3Res"]:
+        changes['correction'] = corr
+
+        settings = plotbase.getsettings(opt, changes=changes, quantity="%s_zpt" % resp)
+
+        rgraph = plotresponse.getresponse(resp, 'zpt', opt, settings, file1, file2)
+        string = "_".join(filter(None, [dtype, d[resp], opt.algorithm.replace("AK5PFJets", ""), cu_str, et_str, corr]))
+        rgraph.SetTitle(string)
+        rgraph.SetName(string)
+        rgraph.Write()
+        rgraph.Delete()
 
 
 def mikko_oneset(files, opt, types=['bal', 'mpf', 'mpf-raw']):
-    et_strings = ["eta00_13", "eta00_08","eta08_13", "eta13_19", "eta19_25", "eta25_30", "eta30_32", "eta32_52"]
-    cu_strings = ["a10", "a15", "a20", "a30"]
+    et_strings = [
+         "eta00_13", "eta00_08", "eta08_13", "eta13_19",
+         "eta19_25", "eta25_30", "eta30_32", "eta32_52"
+    ]
+    a_strings = ["a10", "a15", "a20", "a30", "a40"]
 
+    et_cuts = ["1"] + getroot.etacuts(opt.eta)
+    a_cuts = getroot.alphacuts([0.1, 0.15, 0.2, 0.3, 0.4])
 
-    for et, et_str in zip(([""]+getroot.etastrings(opt.eta)), et_strings):
-        for cu, cu_str in zip(getroot.cutstrings(opt.cut), cu_strings):
+    for et, et_str in zip(et_cuts, et_strings):
+        for a, a_str in zip(a_cuts, a_strings):
             for t in types:
-                if 'Gen' not in t:
-                    doPlot(t, "data", opt, cu, et, cu_str, et_str, files[0])
-                    doPlot(t, "ratio", opt, cu, et, cu_str, et_str, files[0], files[1])
-                doPlot(t, "mc", opt, cu, et, cu_str, et_str, files[1])
-    print "Output written to:", opt.out + "/plots_mikko.root"
+                if 'gen' not in t:
+                    doPlot(t, opt.labels[0], opt, a, et, a_str, et_str, files[0])
+                    doPlot(t, "ratio", opt, a, et, a_str, et_str, files[0], files[1])
+                doPlot(t, opt.labels[1], opt, a, et, a_str, et_str, files[1])
 
-
+    print "Output written to:", opt.out + "/" + opt.filename
