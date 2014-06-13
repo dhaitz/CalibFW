7e9d8fa35b911f2f34e97edd3955fdc349dec938
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-06-13 10:04:57 +0200

    Move the AddGlobalMetaProducers to Factory.h

--- src/artus.cc
+++ src/artus.cc
@@ -31,21 +31,8 @@
 #include "EventPipeline/EventPipelineRunner.h"
 #include "ZJetEventPipeline/Pipeline.h"
 #include "ZJetEventPipeline/EventProvider.h"
-#include "ZJetProducer/MetadataProducer.h"
-#include "ZJetProducer/Cuts.h"
-#include "ZJetProducer/WeightProducer.h"
-#include "ZJetProducer/PileupTruthProducer.h"
-#include "ZJetProducer/JetCorrector.h"
-#include "ZJetProducer/MuonCorrector.h"
-#include "ZJetProducer/JetSorter.h"
-#include "ZJetProducer/HltSelector.h"
-#include "ZJetProducer/JetMatcher.h"
-#include "ZJetProducer/TypeIMETProducer.h"
-#include "ZJetProducer/UnclusteredEnergyProducer.h"
-#include "ZJetProducer/FlavourProducer.h"
-#include "ZJetProducer/LeadingJetUncertaintyProducer.h"
 #include "ZJetEventPipeline/PipelineInitializer.h"
-#include "ZJetProducer/ZeeProducer.h"
+#include "ZJetEventPipeline/Factory.h"
 
 #include "KappaTools/RootTools/HLTTools.h"
 
@@ -72,82 +59,6 @@ TFile* g_resFile;
 typedef std::vector<ZJetPipelineSettings*> PipelineSettingsVector;
 
 PipelineSettingsVector g_pipeSettings;
-
-
-void AddGlobalMetaProducer(std::vector<std::string> const& producer,
-						   EventPipelineRunner<ZJetPipeline, ZJetGlobalMetaDataProducerBase>& runner,
-						   boost::property_tree::ptree& globalSettings)
-{
-	// extend here, if you want to provide a new global meta producer
-	for (std::vector<std::string>::const_iterator it = producer.begin();
-		 it != producer.end(); ++it)
-	{
-		std::string mucorr = globalSettings.get<std::string>("MuonCorrectionParameters", "missing");
-		if (ValidMuonProducer::Name() == *it)
-			runner.AddGlobalMetaProducer(new ValidMuonProducer());
-		else if (ZProducer::Name() == *it)
-			runner.AddGlobalMetaProducer(new ZProducer());
-		else if (WeightProducer::Name() == *it)
-			runner.AddGlobalMetaProducer(new WeightProducer(globalSettings.get<std::string>("PileupWeights")));
-		else if (PileupTruthProducer::Name() == *it)
-			runner.AddGlobalMetaProducer(new PileupTruthProducer(
-											 globalSettings.get<std::string>("PileupTruth"),
-											 globalSettings.get<double>("MinBiasXS")));
-		else if (MuonCorrector::Name() == *it)
-			runner.AddGlobalMetaProducer(new MuonCorrector(
-											 mucorr,
-											 globalSettings.get<std::string>("MuonCorrectionParametersRunD", mucorr),
-											 globalSettings.get<bool>("MuonSmearing", false),
-											 globalSettings.get<bool>("MuonRadiationCorrection", false)));
-		else if (ValidJetProducer::Name() == *it)
-			runner.AddGlobalMetaProducer(new ValidJetProducer(
-											 globalSettings.get<bool>("Tagged")));
-		else if (ValidJetEEProducer::Name() == *it)
-			runner.AddGlobalMetaProducer(new ValidJetEEProducer(
-											 globalSettings.get<bool>("Tagged")));
-		else if (JetCorrector::Name() == *it)
-			runner.AddGlobalMetaProducer(new JetCorrector(
-											 globalSettings.get<std::string>("Jec"),
-											 globalSettings.get<std::string>("L1Correction"),
-											 PropertyTreeSupport::GetAsStringList(&globalSettings, "GlobalAlgorithms"),
-											 globalSettings.get<bool>("RC")));
-		else if (JetSorter::Name() == *it)
-			runner.AddGlobalMetaProducer(new JetSorter());
-		else if (HltSelector::Name() == *it)
-			runner.AddGlobalMetaProducer(new HltSelector(PropertyTreeSupport::GetAsStringList(&globalSettings, "HltPaths", true)));
-		else if (JetMatcher::Name() == *it)
-			runner.AddGlobalMetaProducer(new JetMatcher(PropertyTreeSupport::GetAsStringList(&globalSettings, "GlobalAlgorithms")));
-		else if (GenMetProducer::Name() == *it)
-			runner.AddGlobalMetaProducer(new GenMetProducer());
-		else if (GenProducer::Name() == *it)
-			runner.AddGlobalMetaProducer(new GenProducer());
-		else if (GenBalanceProducer::Name() == *it)
-			runner.AddGlobalMetaProducer(new GenBalanceProducer());
-		else if (GenDibalanceProducer::Name() == *it)
-			runner.AddGlobalMetaProducer(new GenDibalanceProducer());
-		else if (TypeIMETProducer::Name() == *it)
-			runner.AddGlobalMetaProducer(new TypeIMETProducer(
-											 globalSettings.get<bool>("EnableMetPhiCorrection"),
-											 PropertyTreeSupport::GetAsStringList(&globalSettings, "GlobalAlgorithms"),
-											 globalSettings.get<bool>("RC")));
-
-		else if (UnclusteredEnergyProducer::Name() == *it)
-			runner.AddGlobalMetaProducer(new UnclusteredEnergyProducer(
-											 PropertyTreeSupport::GetAsStringList(&globalSettings, "GlobalAlgorithms")));
-		else if (FlavourProducer::Name() == *it)
-			runner.AddGlobalMetaProducer(new FlavourProducer());
-		else if (LeadingJetUncertaintyProducer::Name() == *it)
-			runner.AddGlobalMetaProducer(new LeadingJetUncertaintyProducer(
-											 globalSettings.get<std::string>("Jec"),
-											 PropertyTreeSupport::GetAsStringList(&globalSettings, "GlobalAlgorithms")));
-		else if (ZEEProducer::Name() == *it)
-			runner.AddGlobalMetaProducer(new ZEEProducer());
-		else if (ValidElectronProducer::Name() == *it)
-			runner.AddGlobalMetaProducer(new ValidElectronProducer());
-		else
-			LOG_FATAL("Global MetaData producer of name " << *it << " not found");
-	}
-}
 
 
 int main(int argc, char** argv)
@@ -274,7 +185,7 @@ int main(int argc, char** argv)
 		stringvector globalProducer = PropertyTreeSupport::GetAsStringList(
 										  &g_propTree, "GlobalProducer");
 
-		AddGlobalMetaProducer(globalProducer, pRunner, g_propTree);
+		AddGlobalMetaProducers(globalProducer, pRunner, g_propTree);
 
 		for (PipelineSettingsVector::iterator it = g_pipeSettings.begin();
 			 it != g_pipeSettings.end(); it++)
