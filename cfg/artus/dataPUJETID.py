176b11a415d087078e90f8d90c689537840a1dfc
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-07-01 11:46:35 +0200

    Change in pujetid cfg files

--- cfg/artus/dataPUJETID.py
+++ cfg/artus/dataPUJETID.py
@@ -1,13 +1,9 @@
 import ArtusConfigBase as base
+import data
 
 
 def config():
-    conf = base.BaseConfig('data', '2012')
-    #conf["InputFiles"] = "/storage/8/berger/zpj/kappa539_Data12_tags/*.root"
+    conf = data.config()
     conf['VetoPileupJets'] = True
-    conf["InputFiles"] = "/storage/a/dhaitz/skims/2014_03_25_data12/*.root"
-    conf = base.expand(conf, ['all', 'zcuts'])
-    conf['Jec'] = base.getPath() + "/data/jec/Winter14/Winter14_V1_DATA"
-    conf['RC'] = True
     return conf
 
--- cfg/artus/mc_rundepPUJETID.py
+++ cfg/artus/mc_rundepPUJETID.py
@@ -1,15 +1,9 @@
 import ArtusConfigBase as base
+import mc_rundep
 
 
 def config():
-    conf = base.BaseConfig('mc', '2012', rundepMC=True)
-    conf["InputFiles"] = "/storage/a/berger/zpj/kappa5313_MC12_madgraph_rundep-2/*.root"
-    conf = base.expand(conf, ['all', 'zcuts'])
-    conf['Jec'] = base.getPath() + "/data/jec/Winter14/Winter14_V1_MC"
-    conf['PileupWeights'] = base.getPath() + "/data/pileup/weights_190456-208686_8TeV_22Jan2013ReReco_kappa5313_MC12_madgraph_rundep-2.root"
-    conf['checkKappa'] = "newtagging"
-    conf['RC'] = True
+    conf = mc_rundep.conf()
     conf['VetoPileupJets'] = True
-    #conf['checkArtus'] = "65a2c8c2"
     return conf
 
