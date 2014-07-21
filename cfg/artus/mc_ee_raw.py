908a9fc2c40cdac851c31fecec898f70a2bfb3df
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-07-21 11:16:22 +0200

    ee MC: Switch to the latest skim (RD-MC)

--- cfg/artus/mc_ee_raw.py
+++ cfg/artus/mc_ee_raw.py
@@ -2,8 +2,9 @@ import ArtusConfigBase as base
 
 
 def config():
-    conf = base.BaseConfig('mc', '2012', analysis='zee')
-    conf["InputFiles"] = "/storage/a/dhaitz/skims/2014_06_23_zee_mc_madgraph/*.root"
+    conf = base.BaseConfig('mc', '2012', analysis='zee', rundepMC=True)
+    #conf["InputFiles"] = "/storage/a/dhaitz/skims/2014_06_23_zee_mc_madgraph/*.root"
+    conf["InputFiles"] = "/storage/a/dhaitz/skims/2014_07_18_mc_ee_mm/*.root"
     conf['EnableLumiReweighting'] = True
     conf['EnableTriggerReweighting'] = False
     conf['NEvents'] = 30459503
