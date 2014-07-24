2466fe53c145bb34510beca7532767f0f72bc436
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-07-24 11:32:26 +0200

    Adjust cfg to latest skim

--- cfg/artus/data_ee.py
+++ cfg/artus/data_ee.py
@@ -1,9 +1,10 @@
 import ArtusConfigBase as base
 import socket
 
+
 def config():
     conf = base.BaseConfig('data', '2012', analysis='zee')
-    conf["InputFiles"] = "/storage/a/dhaitz/skims/2014_02_14_zee_data/*.root"
+    conf["InputFiles"] = "/storage/a/dhaitz/skims/2014_06_23_zee_data/*.root"
     #conf['GlobalProducer'].remove('hlt_selector')
     #conf['Pipelines']['default']['Filter'].remove('hlt')
     conf = base.expand(conf, ['all', 'zcuts'])
