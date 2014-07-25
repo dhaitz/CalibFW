027198d9e33bbc0e0c793f7dd9174f61f084dfb3
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-07-25 10:38:42 +0200

    Updated ee powheg cfg

--- cfg/artus/mc_ee_powheg.py
+++ cfg/artus/mc_ee_powheg.py
@@ -3,11 +3,12 @@ import ArtusConfigBase as base
 
 def config():
     conf = base.BaseConfig('mc', '2012', analysis='zee', lhe=True)
-    conf["InputFiles"] = "/storage/a/dhaitz/skims/2014_02_28_zee_mc_powheg/*.root"
+    conf["InputFiles"] = "/storage/a/dhaitz/skims/2014_07_24_zee_mc_powheg/*.root"
     conf['EnableLumiReweighting'] = True
     conf['EnableTriggerReweighting'] = False
-    conf['NEvents'] = 42718644
+    conf['NEvents'] = 3297045
     conf['XSection'] = 1966.7
+    conf['PileupWeights'] = base.getPath() + "/data/pileup/weights_190456-208686_8TeV_22Jan2013ReReco_2014_07_24_zee_powheg.root"
     conf = base.expand(conf, ['all', 'zcuts'])
     return conf
 
