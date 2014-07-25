5f2d0c36b2b9f97eb766d91f278958803c0a1f90
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-07-25 14:30:05 +0200

    Add HLT selector and producer (and path) by default for ee MC

--- cfg/artus/mc_ee_corr_trig.py
+++ cfg/artus/mc_ee_corr_trig.py
@@ -5,8 +5,6 @@ import mc_ee_raw
 def config():
     conf = mc_ee_raw.config()
     conf['electrons'] = 'correlectrons'
-    conf['HltPaths'] = ["HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v%d" % v for v in range(15, 20)]
-    conf['GlobalProducer'] += ['hlt_selector']
     for i in conf['Pipelines']:
         conf['Pipelines'][i]['Filter'].append('hlt')
     return conf
--- cfg/artus/mc_ee_raw.py
+++ cfg/artus/mc_ee_raw.py
@@ -11,6 +11,10 @@ def config():
 
     conf['PileupWeights'] = base.getPath() + "/data/pileup/weights_190456-208686_8TeV_22Jan2013ReReco_2014_06_23_zee_madgraph.root"
 
+    conf['HltPaths'] = ["HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v%d" % v for v in range(15, 20)]
+    conf['GlobalProducer'] += ['hlt_selector']
+    conf['Pipelines']['default']['QuantitiesVector'].append('hlt')
+
     conf = base.expand(conf, ['all', 'zcuts'])
     return conf
 
