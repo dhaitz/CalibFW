144182c277d70bed97ba02013477f173d86ac475
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-08-01 11:00:23 +0200

    cfg data: switch to latest residuals by mikko

--- cfg/artus/data.py
+++ cfg/artus/data.py
@@ -6,6 +6,6 @@ def config():
     #conf["InputFiles"] = "/storage/8/berger/zpj/kappa539_Data12_tags/*.root"
     conf["InputFiles"] = "/storage/a/dhaitz/skims/2014_03_25_data12/*.root"
     conf = base.expand(conf, ['all', 'zcuts'])
-    conf['Jec'] = base.getPath() + "/data/jec/Winter14/Winter14_V1_DATA"
+    conf['Jec'] = base.getPath() + "/data/jec/Winter14_V3/Winter14_V3_DATA"
     conf['RC'] = True
     return conf
