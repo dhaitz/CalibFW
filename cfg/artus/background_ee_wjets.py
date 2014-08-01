f6433c2b3b95960b8fe35aac37cefd08b1d769c3
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-08-01 09:59:08 +0200

    cfg: New skim fow WJets sample

--- cfg/artus/background_ee_wjets.py
+++ cfg/artus/background_ee_wjets.py
@@ -3,10 +3,10 @@ import ArtusConfigBase as base
 
 def config():
     conf = base.BaseConfig('mc', '2012', analysis='zee')
-    conf['InputFiles'] = '/storage/a/dhaitz/skims/2014_02_28_zee_backgrounds/kappa_Wjets_*.root'
+    conf['InputFiles'] = '/storage/8/dhaitz/skims/2014_07_29_wjets/*.root'
     conf['EnableLumiReweighting'] = True
     conf['EnableTriggerReweighting'] = False
     conf['NEvents'] = 57709905
-    conf['XSection'] = 36895.8
+    conf['XSection'] = 37509.8
     conf = base.expand(conf, ['all', 'zcuts'])
     return conf
