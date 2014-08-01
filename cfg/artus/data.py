3c8a86ab5801c8815875dd1f5f43d657d2f43b26
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-08-01 10:59:55 +0200

    cfg: Add NAF path for data

--- cfg/artus/data.py
+++ cfg/artus/data.py
@@ -3,8 +3,10 @@ import ArtusConfigBase as base
 
 def config():
     conf = base.BaseConfig('data', '2012')
-    #conf["InputFiles"] = "/storage/8/berger/zpj/kappa539_Data12_tags/*.root"
-    conf["InputFiles"] = "/storage/a/dhaitz/skims/2014_03_25_data12/*.root"
+    conf["InputFiles"] = base.setInputFiles(
+        ekppath = "/storage/a/dhaitz/skims/2014_03_25_data12/*.root",
+        nafpath = "/pnfs/desy.de/cms/tier2/store/user/dhaitz/2014_03_25_data12"
+    )
     conf = base.expand(conf, ['all', 'zcuts'])
     conf['Jec'] = base.getPath() + "/data/jec/Winter14_V3/Winter14_V3_DATA"
     conf['RC'] = True
