8e16d11238490aa80d74dd3dd16241853c1f4c0a
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-08-04 09:49:32 +0200

    Added NAF paths for MC ee

--- cfg/artus/mc_ee_raw.py
+++ cfg/artus/mc_ee_raw.py
@@ -3,7 +3,10 @@ import ArtusConfigBase as base
 
 def config():
     conf = base.BaseConfig('mc', '2012', analysis='zee', rundepMC=True, lhe=True)
-    conf["InputFiles"] = "/storage/a/dhaitz/skims/2014_07_18_mc_ee_mm/*.root"
+    conf["InputFiles"] = base.setInputFiles(
+        ekppath="/storage/9/dhaitz/skims/2014_07_18_ee-mm-mcRD/*.root",
+        nafpath="/nfs/dust/cms/user/dhaitz/skims/2014_07_18_ee-mm-mcRD/*.root"
+    )
     conf['EnableLumiReweighting'] = True
     conf['EnableTriggerReweighting'] = False
     conf['NEvents'] = 30459503
