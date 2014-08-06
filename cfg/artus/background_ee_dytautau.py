8345771fc49bf99eeefa88f09c4fa19801a74a6a
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-08-06 17:54:03 +0200

    New skim location for DYtautau

--- cfg/artus/background_ee_dytautau.py
+++ cfg/artus/background_ee_dytautau.py
@@ -3,10 +3,13 @@ import ArtusConfigBase as base
 
 def config():
     conf = base.BaseConfig('mc', '2012', analysis='zee')
-    conf['InputFiles'] = '/storage/a/dhaitz/skims/2014_02_28_zee_backgrounds/kappa_DYtautau_*.root'
+    conf["InputFiles"] = base.setInputFiles(
+        ekppath="/storage/9/dhaitz/skims/2014_07_31_ee_mc_DYtautau",
+        nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2014_07_31_ee_mc_DYtautau"
+    )
     conf['EnableLumiReweighting'] = True
     conf['EnableTriggerReweighting'] = False
-    conf['NEvents'] = 48790562
+    conf['NEvents'] = 3295238
     conf['XSection'] = 1966.7
     conf = base.expand(conf, ['all', 'zcuts'])
     return conf
