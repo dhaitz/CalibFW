f44e06d28cd134b5b5b6b085be891bbc1d740400
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-08-06 13:31:47 +0200

    Added NAF paths for ttjets cfg

--- cfg/artus/background_ee_tt.py
+++ cfg/artus/background_ee_tt.py
@@ -3,7 +3,10 @@ import ArtusConfigBase as base
 
 def config():
     conf = base.BaseConfig('mc', '2012', analysis='zee', rundepMC=True)
-    conf['InputFiles'] = '/storage/9/dhaitz/skims/2014_08_04_ee_mc_TTJets/*.root'
+    conf["InputFiles"] = base.setInputFiles(
+        ekppath="/storage/9/dhaitz/skims/2014_08_04_ee_mc_TTJets/*.root",
+        nafpath='/pnfs/desy.de/cms/tier2/store/user/dhaitz/2014_08_04_ee_mc_TTJets/',
+    )
     conf['EnableLumiReweighting'] = True
     conf['EnableTriggerReweighting'] = False
     conf['NEvents'] = 21675970
