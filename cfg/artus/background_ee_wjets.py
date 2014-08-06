ae357752cb2ce7755a324c95cb4109e1389f461a
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-08-06 11:42:15 +0200

    Added NAF path for wjets cfg

--- cfg/artus/background_ee_wjets.py
+++ cfg/artus/background_ee_wjets.py
@@ -3,7 +3,10 @@ import ArtusConfigBase as base
 
 def config():
     conf = base.BaseConfig('mc', '2012', analysis='zee')
-    conf['InputFiles'] = '/storage/9/dhaitz/skims/2014_07_29_wjets/*.root'
+    conf["InputFiles"] = base.setInputFiles(
+        ekppath="/storage/9/dhaitz/skims/2014_07_29_wjets/*.root",
+        nafpath="/nfs/dust/cms/user/dhaitz/skims/2014_07_29_ee-mm-wjets/*.root"
+    )
     conf['EnableLumiReweighting'] = True
     conf['EnableTriggerReweighting'] = False
     conf['NEvents'] = 57709905
