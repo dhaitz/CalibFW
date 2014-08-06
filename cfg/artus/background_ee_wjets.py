ac2e6dd62023327c16eb92dd6eb3879ddb9f6a0e
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-08-06 09:28:23 +0200

    Wjets cfg: skim moved to FS9 since FS8 is painfully slow

--- cfg/artus/background_ee_wjets.py
+++ cfg/artus/background_ee_wjets.py
@@ -3,7 +3,7 @@ import ArtusConfigBase as base
 
 def config():
     conf = base.BaseConfig('mc', '2012', analysis='zee')
-    conf['InputFiles'] = '/storage/8/dhaitz/skims/2014_07_29_wjets/*.root'
+    conf['InputFiles'] = '/storage/9/dhaitz/skims/2014_07_29_wjets/*.root'
     conf['EnableLumiReweighting'] = True
     conf['EnableTriggerReweighting'] = False
     conf['NEvents'] = 57709905
