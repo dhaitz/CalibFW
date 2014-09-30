983e24cbdcbcf470740d4c2a55bf8261d56ef298
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-09-30 16:42:55 +0200

    Changes in e-mu cfg

--- cfg/artus/data_emu.py
+++ cfg/artus/data_emu.py
@@ -7,7 +7,5 @@ def config():
         ekppath="",
         nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2014_08_08_data_MuEG"
     )
-    #conf['GlobalProducer'].remove('hlt_selector')
-    #conf['Pipelines']['default']['Filter'].remove('hlt')
-    conf = base.expand(conf, ['all', 'zcuts', 'incut'])
+    conf = base.expand(conf, ['all', 'zcuts'])
     return conf
