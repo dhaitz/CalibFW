26de403f62c9a25e02169d94876f87066f6767eb
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-11-04 09:37:42 +0100

    Special_binning for jet1pt: use same as Z pT

--- plotting/tools/getroot.py
+++ plotting/tools/getroot.py
@@ -175,6 +175,8 @@ def getBinning(quantity, settings, axis='x'):
         # No, opt is the wrong place, -> dict
         bin_dict = {
             'zpt': settings['zbins'],
+            'jet1pt': settings['zbins'],
+            'genjet1pt': settings['zbins'],
             '(abs(jet1eta))': settings['eta'],
             'jet1eta': [-elem for elem in settings['eta'][1:][::-1]] + settings['eta'],
             'npv': [a - 0.5 for a, b in settings['npv']] + [settings['npv'][-1][1] - 0.5]
