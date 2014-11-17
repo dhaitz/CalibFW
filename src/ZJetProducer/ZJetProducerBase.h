a32a43d2228b216b9b9682b02a8d9c71ea7007c8
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2014-11-17 14:13:24 +0100

    Added ZJetProducerBase.h file

---
+++ src/ZJetProducer/ZJetProducerBase.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "ZJetEventPipeline/Pipeline.h"
+
+//class GlobalProductProducerBase;
+
+namespace Artus
+{
+
+typedef GlobalProductProducerBase<ZJetEventData, ZJetProduct, ZJetPipelineSettings> ZJetGlobalProductProducerBase;
+
+}
