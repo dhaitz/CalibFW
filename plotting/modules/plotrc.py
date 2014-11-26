501974836d17ad027df74a47e6793342ba2c502a
Joram Berger <joram.berger@cern.ch>
2014-11-26 17:00:19 +0100

    Better check for fonts

--- plotting/modules/plotrc.py
+++ plotting/modules/plotrc.py
@@ -69,26 +69,44 @@ class MplStyles():
 		'figure.subplot.hspace': 0.2,
 	})
 
+	availableFonts = [f[f.rfind('/') + 1:-4]
+		for f in matplotlib.font_manager.findSystemFonts(fontpaths=None, fontext='ttf')]
+
+	def checkFont(self, fonts, usedfor='', info=False):
+		if type(fonts) == str:
+			fonts = [fonts]
+		for font in fonts:
+			if font not in self.availableFonts:
+				print "! Font %r is not available but used for %r" % (font, usedfor)
+			elif info:
+				print "  Font %r available" % font
+	
+	def checkFonts(self, style):
+		fontsettings = ['font.serif', 'font.sans-serif', 'font.cursive', 'font.monospace']
+		for key in style:
+			if key in fontsettings and style[key]:
+				self.checkFont(style[key], key)
 
 def getstyle(style='cmsstyle_JetMET'):
 	s = MplStyles()
 	if not hasattr(s, style):
 		print "Style %r not defined!" % style
 		return s.defaultstyle
+	st = getattr(s, style)
+	s.checkFonts(st)
 	return getattr(s, style)
 
 
 if __name__ == "__main__":
-    styles = sys.argv[1:]
+    fontsFound = True
     s = MplStyles()
-    availableFonts = matplotlib.font_manager.findSystemFonts(fontpaths=None, fontext='ttf')
-    availableFonts = [f[f.rfind('/') + 1:-4] for f in availableFonts]
-    print availableFonts
+    styles = sys.argv[1:]
     if not styles:
         styles = [d for d in dir(s) if type(getattr(s, d)) == dict]
     for style in styles:
-		print "Style: %s" % style
-		for k, v in getattr(s, style).items():
-			print "  {0:24}: {1}".format(k, repr(v))
-			if k in ['font.serif', 'font.sans-serif', 'font.cursive', 'font.monospace'] and v not in availableFonts:
-				print "       Font %s not available!" % v
+        d = getattr(s, style)
+        print "Style: %s" % style
+        fontsFound = fontsFound and s.checkFonts(d)
+        for k, v in d.items():
+            print "  {0:24}: {1}".format(k, repr(v))
+
