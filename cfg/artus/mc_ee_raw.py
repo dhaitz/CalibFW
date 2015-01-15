42122293b419299e6f4bdb204b8562d084cc7cca
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2015-01-15 16:03:16 +0100

    Change ee MC cfg to latest skim

--- cfg/artus/mc_ee_raw.py
+++ cfg/artus/mc_ee_raw.py
@@ -5,14 +5,14 @@ def config():
     conf = base.BaseConfig('mc', '2012', analysis='ee', rundepMC=True, lhe=True)
     conf["InputFiles"] = base.setInputFiles(
         ekppath="/storage/9/dhaitz/skims/2014_07_18_ee-mm-mcRD/*.root",
-        nafpath="/nfs/dust/cms/user/dhaitz/skims/2014_07_18_ee-mm-mcRD/*.root"
+        nafpath="/pnfs/desy.de/cms/tier2/store/user/dhaitz/2015_01_12_ee_mc_madgraphRD/kappa_madgraphRD_*.root"
     )
     conf['EnableLumiReweighting'] = True
     conf['EnableTriggerReweighting'] = False
     conf['NEvents'] = 30458871
     conf['XSection'] = 3503.71
 
-    conf['PileupWeights'] = base.getPath() + "/data/pileup/weights_190456-208686_8TeV_22Jan2013ReReco_2014_06_23_zee_madgraph.root"
+    conf['PileupWeights'] = base.getPath() + "/data/pileup/weights_190456-208686_8TeV_22Jan2013ReReco_2015_01_12_ee_madgraphRD.root"
 
     conf = base.expand(conf, ['all', 'zcuts', 'incut'])
     return conf
